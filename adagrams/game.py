d07c8f45b38d4f8e9275ea5129e6eb87e49bdde2
Tanil <cadette.tanil@gmail.com>
2022-09-26 16:55:34 -0400

    completed wave 01 and passed all test

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,53 @@
+import random
+LETTER_POOL = [{
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}]
 def draw_letters():
-    pass
+    letters = []
+    remaining_letters_pool = {}
+    
+    if len(letters) < 11:
+        for dict in LETTER_POOL:
+            for key, value in dict.items():
+                if key not in remaining_letters_pool and value > 0:
+                    remaining_letters_pool[key] = value
+  
+    while len(letters) < 10:
+        random_letter = random.choice(list(remaining_letters_pool))
+        for letter in list(remaining_letters_pool):
+            if remaining_letters_pool[letter] == 0:
+                remaining_letters_pool.pop(letter)
+            elif random_letter == letter:
+                remaining_letters_pool[letter]-= 1
+                letters.append(random_letter)
+                
+    return letters
+    
 
 def uses_available_letters(word, letter_bank):
     pass
