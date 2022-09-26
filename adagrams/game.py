2b3e79c7a752372d15afe1115d2c7dc7d6f31891
Liat <liathirsh@gmail.com>
2022-09-26 17:01:03 -0400

    draw_letters complete untested

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,22 @@
+from tests.test_wave_01 import LETTER_POOL
+import random
+import copy
+
 def draw_letters():
-    pass
+
+    letter_bank = []
+    temp_letter_pool = copy.deepcopy(LETTER_POOL)
+
+    while len(letter_bank) < 10:        
+        letter = random.choice(list(temp_letter_pool.keys()))
+        if temp_letter_pool[letter] > 0:
+            temp_letter_pool[letter] -= 1
+            letter_bank.append(letter)
+        else:
+            continue
+   
+    return letter_bank
+
 
 def uses_available_letters(word, letter_bank):
     pass
