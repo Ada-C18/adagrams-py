7c1ac3bd7736fe7a1c83fda52b7fe13a6c87ac2d
Sunny <1sunnymuniz@gmail.com>
2022-09-26 15:32:36 -0600

    modified function draw_letters, tests 1 and 2 in Wave 01 are now passing

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,49 @@
+import random
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+
 def draw_letters():
+    word_pool_list = []
+    random_string = [] 
+
+    for letter in LETTER_POOL:
+        for num in range(0,(LETTER_POOL[letter])):
+            word_pool_list.append(letter)
+
+
+    for num in range(0,10):
+        random_letter = random.choice(word_pool_list)
+        random_string.append(random_letter)
     
-    pass
+    
+    return random_string
 
 def uses_available_letters(word, letter_bank):
     pass
