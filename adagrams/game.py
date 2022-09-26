ba9e0b2c698291af5a6291173dc3d7c7d7cde4a2
Uche Audrey <uaudrey3@gmail.com>
2022-09-26 13:29:42 -0400

    pseudocode won't be pushed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,42 @@
+import random
+
+LETTERS = {
+    "A": 9,
+    "B": 2,
+    "C": 2,
+    "D": 4,
+    "E": 12,
+    "F": 2,
+    "G": 3,
+    "H": 2,
+    "I": 9,
+    "J": 1,
+    "K": 1,
+    "L": 4,
+    "M": 2,
+    "N": 6,
+    "O": 8,
+    "P": 2,
+    "Q": 1,
+    "R": 6,
+    "S": 4,
+    "T": 6,
+    "U": 4,
+    "V": 2,
+    "W": 2,
+    "X": 1,
+    "Y": 2,
+    "Z": 1
+}
+
 def draw_letters():
-    pass
+    pool = []
+    
+    for key, value in LETTERS.items():
+        pool += (key * value)
+    
+    user_letters = random.choices(pool, k = 10)
+
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +46,6 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+letters = []
+for key, value in dict.items():
