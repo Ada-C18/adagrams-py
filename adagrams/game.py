ec4b1745c99309aa46516fc838245ffe1d2f6caa
Michelle Oh <ohmotrl@gmail.com>
2022-09-26 11:55:22 -0400

    added comments while brainstorming

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,16 @@
+import random
+
 def draw_letters():
-    print("hello, world!")
+    # return an array of ten strings
+        # each str should contain exactly 1 letter
+        # these represent the hand of letters that the player has drawn
+    # the letters should be randomly drawn from LETTER_POOL
+        # This letter pool should reflect the distribution of letters as described in the table below
+        # There are only 2 available C letters, so draw_letters cannot ever return more than 2 Cs
+        # Since there are 12 Es but only 1 Z, it should be 12 times as likely for the user to draw an 
+        # E as a Z
+    # Invoking this function should not change the pool of letters
+        # Imagine that the user returns their hand to the pool before drawing new letters
 
 def uses_available_letters(word, letter_bank):
     print("testing git")
