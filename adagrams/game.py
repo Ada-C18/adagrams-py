27123b574f644c2c01b0352b0b07c2b0d4e4c2a2
Margaret <y.margaret.lu+github@gmail.com>
2022-09-26 13:34:07 -0700

    Test push for install multiset

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,30 @@
+import random
+import string
+
 def draw_letters():
-    pass
+    letter_dict={
+        'A' : 9, 'N' : 6,
+        'B' : 2, 'O' : 8,
+        'C' : 2, 'P' : 2,
+        'D' : 4, 'Q' : 1,
+        'E' : 12, 'R' : 6,
+        'F' : 2, 'S' : 4,
+        'G' : 3, 'T' : 6,
+        'H' : 2, 'U' : 4,
+        'I' : 9, 'V' : 2,
+        'J' : 1, 'W' : 2,
+        'K' : 1, 'X' : 1,
+        'L' : 4, 'Y' : 2,
+        'M' : 2, 'Z' : 1 }
+    letter_list=[letter for letter in string.ascii_uppercase]
+    representative_letter_list=[]
+    for chara in letter_list:
+        for counter in range(letter_dict[chara]):
+            representative_letter_list.append(chara)
+    return_list=[]
+    for count in range(10):
+        return_list.append(representative_letter_list.pop(random.randint(0,len(representative_letter_list)-1)))
+    return return_list
 
 def uses_available_letters(word, letter_bank):
     pass
