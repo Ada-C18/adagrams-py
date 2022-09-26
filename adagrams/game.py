e2444196fdccd082dc9903343fb704da7b12f7f7
Tazmeen <t.akhtar95@gmail.com>
2022-09-26 14:41:36 -0400

    ..

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,11 +59,7 @@ def draw_letters():
             break
 
     return letter_bank
-    
-        
 
-
-draw_letters()
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -74,17 +70,3 @@ def score_word(word):
 def get_highest_word_score(word_list):
     pass
 
-
-# | A : 9  | N : 6 |
-# | B : 2  | O : 8 |
-# | C : 2  | P : 2 |
-# | D : 4  | Q : 1 |
-# | E : 12 | R : 6 |
-# | F : 2  | S : 4 |
-# | G : 3  | T : 6 |
-# | H : 2  | U : 4 |
-# | I : 9  | V : 2 |
-# | J : 1  | W : 2 |
-# | K : 1  | X : 1 |
-# | L : 4  | Y : 2 |
-# | M : 2  | Z : 1 |
