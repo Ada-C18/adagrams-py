6fa02ae7c06b296d2a13e7e3291192e0db9a4c12
Cristal <cbcodee@gmail.com>
2022-09-26 12:01:06 -0700

    Started on wave one

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,45 @@
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
+    res = []
+    letters = []
+    for k,v in LETTER_POOL.items():
+        while v > 0:
+            letters.append(k)
+            v -=1
+    for i in range(10):
+        # hoping to get a random integer between 0 and 97
+        index = random.randrange(stop) 
+        # check if index in our letters is used/empty, if not, get another random integer
+        
+        # append     
     pass
 
 def uses_available_letters(word, letter_bank):
