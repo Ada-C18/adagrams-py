56c9d9d8b53bc4135bb5fb8f52ecab88c36f97b6
Milena Souto <yamadasouto@gmail.com>
2022-09-26 12:07:59 -0700

    Add comments to game file

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,18 @@
+# WAVE 1
 def draw_letters():
     pass
 
+
+# WAVE 2
 def uses_available_letters(word, letter_bank):
     pass
 
+
+# WAVE 3
 def score_word(word):
     pass
 
+
+# WAVE 4
 def get_highest_word_score(word_list):
     pass
