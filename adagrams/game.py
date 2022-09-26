c0d4a2c6134c9cc98dd3d7b5e8f3be8ce1c6b844
Neida <neida@duck.com>
2022-09-26 16:53:08 -0400

    updating pseudocode

--- adagrams/game.py
+++ adagrams/game.py
@@ -10,7 +10,7 @@ def draw_letters():
         # go into the index of editable copy of list
         # make a random integer between (1-the value) held within previous dictionary index
         # append the letter however many times that random integer dictates
-    # return list
+    # return list 
 
     letter_pool = [{"A" : 9}, {"N" : 6}, {"B" : 2}, {"O" : 8}, {"C" : 2}, {"P" : 2}, {"D" : 4}, {"Q" : 1}, {"E" : 12}, {"R" : 6}, {"F" : 2}, {"S" : 4}, {"G": 3}, {"T" : 6}, {"H" : 2}, {"U" : 4}, {"I" : 9},  {"V" : 2}, {"J" : 1}, {"W" : 2}, {"K" : 1}, {"X" : 1}, {"L" : 4}, {"Y" : 2}, {"M" : 2}, {"Z" : 1}]
 
