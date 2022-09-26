ed4e3d05c108bea6af354e3a66f1480b292eabbe
Niloofar Nayebi <nil_nyb@yahoo.com>
2022-09-26 19:29:55 -0400

    pass wavw 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -7,19 +7,6 @@ import random
 from tests.test_wave_01 import LETTER_POOL
 import random
 
-def draw_letters():
-    p=[]
-    g=[]
-    for key, value in LETTER_POOL.items():
-        t=key*value
-        g.append(t)
-    #print(g)
-    for i in range(len(g)):
-        for j in g[i]:
-            p.append(g[i][0])
-    #print(p)
-    draw_letters=(random.sample(p,10))
-    
     return draw_letters   
     
 =======
@@ -77,8 +64,6 @@ def uses_available_letters(word, letter_bank):
         return True
     else:
         return False
-    
-    
     
     
     
