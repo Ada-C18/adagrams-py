73b426481f4a783b105d865433d1a3a16dbf549f
Tazmeen <t.akhtar95@gmail.com>
2022-09-26 13:15:05 -0400

    didn't make any changes

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,10 @@
-# this is the first function 
+import random 
+
 
 def draw_letters():
     pass
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -11,3 +14,18 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+
+# | A : 9  | N : 6 |
+# | B : 2  | O : 8 |
+# | C : 2  | P : 2 |
+# | D : 4  | Q : 1 |
+# | E : 12 | R : 6 |
+# | F : 2  | S : 4 |
+# | G : 3  | T : 6 |
+# | H : 2  | U : 4 |
+# | I : 9  | V : 2 |
+# | J : 1  | W : 2 |
+# | K : 1  | X : 1 |
+# | L : 4  | Y : 2 |
+# | M : 2  | Z : 1 |
