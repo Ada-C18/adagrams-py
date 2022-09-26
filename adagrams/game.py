6ea94db5c698743b007e9b5a84aaa38738d682ff
Kate <varindakate@gmail.com>
2022-09-26 12:59:15 -0700

    1) added pseudocode to draw_letters function 2) plan of action added to README

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,13 @@
+
 def draw_letters():
     pass
+    # create a list (ORIGINAL_POOL) with all the given letters
+    #LOOP:
+        # create deep copy of ORIGINAL_POOL (GAME_POOL)
+        # use random.choice(list) function to draw from pool (GAME_POOL) of letters
+        # make another list for player's hand (HAND)
+        # once letter is picked remove letter from pool list
+        # use while loop max the letters of player's hand to 10
 
 def uses_available_letters(word, letter_bank):
     pass
