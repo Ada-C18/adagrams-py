b1fb51637c3d8955118ff705b5faa87e11abf3ee
Abby <abbychoi97@gmail.com>
2022-09-26 11:57:55 -0700

    created build_letter_pool to create letter pool

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,46 @@
+
+def build_letter_pool():
+    
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+    }
+
+    letter_pool = []
+
+    for letter, frequency in LETTER_POOL.items():
+        for num in range(frequency):
+            letter_pool.append(letter)
+    print(letter_pool)
+    return letter_pool
+    
+
 def draw_letters():
+    # letter_pool = 
     pass
 
 def uses_available_letters(word, letter_bank):
@@ -9,3 +51,5 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+build_letter_pool()
