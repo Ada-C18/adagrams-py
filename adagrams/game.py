80cee1df7e3fb01072f1b3a41881508c2de2afd9
Masha <iiureva.mariia@gmail.com>
2022-09-26 13:15:22 -0700

    Adding LETTERS-dictionary to list loop for the draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,45 @@
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+
 def draw_letters():
     #this is the first comment ever!
-    pass
+    list_of_all_letter = []
+    for key, value in LETTER_POOL.items():
+        for i in range(value):
+            list_of_all_letter.append(key)
+    
+    print("here's the list: ", list_of_all_letter)
+
+list = draw_letters()
+    
+
 
 def uses_available_letters(word, letter_bank):
     pass
