e72501604b21ae55071dbd27be96429f552c30cc
ashanti <ammanier2@gmail.com>
2022-09-26 12:35:58 -0400

    added letter pool constant and pseudocode

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,48 @@
-# this is the first function 
+### Wave 1: draw_letters
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+#letter_bank: list of 10 letter for the player
+#word: word user comes up with
+#word_list: list of words user comes up with
 
 def draw_letters():
+#Create LETTER_POOL variable 
+#   houses letters and availability
+#Randomly pull 10 letters from letter pool
+#   store in variable: letters
+#   list of strings
+    # letter not selected too many times
+#       keep track of letter availability
+#       decrement every time letter is drawn
+#letter_pool resets after every round
     pass
 
 def uses_available_letters(word, letter_bank):
