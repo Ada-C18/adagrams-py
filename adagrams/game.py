a0053b3236530a67cd284d7aab5bb90ca8356f84
Andrea <andreagalindo.guerra@gmail.com>
2022-09-26 13:47:14 -0400

    modified function draw_letters in game.py and it passes the first two tests in wave_1

--- adagrams/game.py
+++ adagrams/game.py
@@ -11,7 +11,34 @@ def draw_letters():
         # E as a Z
     # Invoking this function should not change the pool of letters
         # Imagine that the user returns their hand to the pool before drawing new letters
-    
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
     letters = []
     for i in range(10):
         letters.append(random.choice(list(LETTER_POOL.keys())))
@@ -23,6 +50,8 @@ def draw_letters():
     for letter, counts in dict_letters.items():
         if counts > LETTER_POOL[letter]:
             print(f"not enough letters {letter}")
+
+    return letters
 
 def uses_available_letters(word, letter_bank):
     print("testing git")
