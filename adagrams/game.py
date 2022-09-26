7934a3cd9233dd9a6f77de46c1e28f29508cf3ae
Steffanie <lopezsteffanie8@gmail.com>
2022-09-26 10:16:05 -0700

    Passed test_wave_02

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,10 +2,27 @@ def draw_letters():
     pass
 
 def uses_available_letters(word, letter_bank):
-    pass
+    word_list = list(word.upper())
+    letter_bank_copy = letter_bank.copy()
+    return_list = []
+    for letter in word_list:
+        if letter in letter_bank_copy:
+            return_list.append(letter)
+            letter_bank_copy.remove(letter)
+
+    if word_list == return_list:
+        return True
+    else:
+        return False
 
 def score_word(word):
-    pass
+    value_1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+    value_2 = ['D', 'G']
+    value_3 = ['B', 'C', 'M', 'P']
+    value_4 = ['F', 'H', 'V', 'W', 'Y']
+    value_5 = ['K']
+    value_8 = ['J', 'X']
+    value_10 = ['Q', 'Z']
 
 def get_highest_word_score(word_list):
     pass
