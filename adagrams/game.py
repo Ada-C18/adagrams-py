fce94ef47e005ea39ef5dc8aa25e0d849e6d55b6
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-09-26 13:44:29 -0700

    psuedocode for Wave 1 in draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,16 @@
+# Make POOL_OF_LETTERS dictionary (it's a constant variable) based on the README
+
 def draw_letters():
+    # Make an empty list called letter_bank
+    # Create empty dictionary called letter_count_dict
+    # develop for loop that iterates over sequence 10 times and appends to letter_bank
+        # assign letter to random.choice taking in the keys of the POOL_OF_LETTERS dictionary
+            # if letter doesn't match a key in letter_count_dict then add it with value of 1
+            # elif letter matches
+                # Check if value in letter_count_dict is less than POOL_OF_LETTERS
+                    # If True, add one count to letter_count_dict
+                    # Else, minus one from "i" or whatever our for loop variable is 
+    # return letter_bank
     pass
 
 def uses_available_letters(word, letter_bank):
