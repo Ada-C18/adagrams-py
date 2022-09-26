4e0e09a0a931fc614e357417acfb1baa9219c0a0
Rahel Kemal <rahelskemal@gmail.com>
2022-09-26 14:09:20 -0700

    modified anagrams

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,30 +1,5 @@
-def draw_letters():
-    '''
-    
-    ''' 
-    # make letter pool which is a list of dictionaries
-    # make an editable copy of the letter pool
-    # set up an empty list that will hold the hand for the player
-    # loop through the range 1-10
-        # make a varaible that holds a random integer (1-26)
-        # go into the index of editable copy of list
-        # make a random integer between (1-the value) held within previous dictionary index
-        # append the letter however many times that random integer dictates
-    # return list
-
-    letter_pool = [{"A" : 9},  | N : 6 |
-| B : 2  | O : 8 |
-| C : 2  | P : 2 |
-| D : 4  | Q : 1 |
-| E : 12 | R : 6 |
-| F : 2  | S : 4 |
-| G : 3  | T : 6 |
-| H : 2  | U : 4 |
-| I : 9  | V : 2 |
-| J : 1  | W : 2 |
-| K : 1  | X : 1 |
-| L : 4  | Y : 2 |
-| M : 2  | Z : 1 |
+def draw_letters(): 
+    pass
 
 def uses_available_letters(word, letter_bank):
     pass
