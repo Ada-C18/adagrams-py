c1da8efe25e497684e5f0896e738c7b725833edb
Niloofar Nayebi <nil_nyb@yahoo.com>
2022-09-26 19:26:06 -0400

    niloo changes

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,44 @@
+import sys
+from adagrams.ui_helper import *
+from adagrams.game import draw_letters, uses_available_letters, score_word, get_highest_word_score
+import random
+
+from tests.test_wave_01 import LETTER_POOL
+import random
+
 def draw_letters():
-    pass
+    p=[]
+    g=[]
+    for key, value in LETTER_POOL.items():
+        t=key*value
+        g.append(t)
+    #print(g)
+    for i in range(len(g)):
+        for j in g[i]:
+            p.append(g[i][0])
+    #print(p)
+    draw_letters=(random.sample(p,10))
+    
+    return draw_letters   
+    
 
 def uses_available_letters(word, letter_bank):
-    pass
+    letter_bank=draw_letters()
+    word_list=[]
+    for each in word:
+        alphas=each
+        word_list.append(alphas)
+    
+    if all(item in word_list for item in letter_bank ):
+        return True
+    else:
+        return False
+    
+    
+    
+    
+    
+    
 
 def score_word(word):
     pass
