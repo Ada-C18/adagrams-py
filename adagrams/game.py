15d4b0ca7aaf9a153b48b4a36a9b4c2615d65848
Rose Le <rosehle@protonmail.com>
2022-09-26 13:53:33 -0700

    Established score chart for wave_03

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,6 +29,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+SCORE_CHART = {
+    'A': 1,
+    'E': 1, 
+    'I': 1, 
+    'O': 1, 
+    'U': 1, 
+    'L': 1, 
+    'N': 1, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1,
+    'D': 2, 
+    'G': 2,
+    'B': 3, 
+    'C': 3, 
+    'M': 3, 
+    'P': 3,
+    'F': 4, 
+    'H': 4, 
+    'V': 4, 
+    'W': 4, 
+    'Y': 4,
+    'K': 5,
+    'J': 8, 
+    'X': 8,
+    'Q': 10, 
+    'Z': 10
+}
+
 def create_letter_pool_list(LETTER_POOL):
     letter_pool_list = []
     for key, value in LETTER_POOL.items():
