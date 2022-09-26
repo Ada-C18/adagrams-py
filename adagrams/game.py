db0948a0dd5f86212e95327faeaafc80859c1964
lisafeng <lisafenglisa@yahoo.com>
2022-09-26 13:16:05 -0400

    Finished first function and passed wave_01

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,43 @@
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 def draw_letters():
-    pass
+    letters = []
+    letter_pool_dict = LETTER_POOL.copy()
+    while len(letters) < 10:
+        letter = random.choice(list(letter_pool_dict.keys()))
+        for key in letter_pool_dict:
+            if key == letter and letter_pool_dict[key] > 0:
+                letters.append(letter)
+                letter_pool_dict[key] -= 1
+    return letters
 
 def uses_available_letters(word, letter_bank):
     pass
