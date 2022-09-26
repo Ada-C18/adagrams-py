c9a0707794496093fdeb92e14820d7eeb7e3c5a5
Lynn <lynn@jansheski.com>
2022-09-26 16:47:44 -0700

    completed draw_letters function. Passed 3 tests in wave 1 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,46 @@
+# Tests
+# draw 10 letters from letterpool
+# list of 10 letters is letter strings
+# letter not selected too many times
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+
 def draw_letters():
-    pass
+    letters = []
+    for i in range(10):
+        letter = random.choice(list(LETTER_POOL.keys()))
+        if letters.count(letter) < LETTER_POOL[letter]:
+            letters.append(letter)
+    return letters
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +50,5 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+draw_letters()
