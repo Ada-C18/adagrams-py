2154f68feafa0fc461e2abf24b32c735cfed3ae1
wanjunlan <wanjunlan@gmail.com>
2022-09-27 13:57:42 -0700

    passed wave 1 and 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,24 +1,87 @@
 import random
+from collections import Counter
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 
 def draw_letters():
-    hand_letters = []
-    letters=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-            'O','P','Q','R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z']
-    for i in range(10):
-        b=random.randint(0,26)
-        hand_letters.append(letters[b])
+    letter_pool_dict = LETTER_POOL.copy()
+    letter_bank = []
+    while len(letter_bank) < 10:
+        letter = random.choice(list(letter_pool_dict.keys()))
+        if letter_pool_dict[letter] > 0:
+            letter_bank.append(letter)
+            letter_pool_dict[letter] -= 1
+
+    return letter_bank
+        
+
+
+def uses_available_letters(word,letter_bank):
+    upper_case_word = word.upper()
+    for letter in upper_case_word:
+        if letter not in letter_bank:
+            return False
+        elif upper_case_word.count(letter) > letter_bank.count(letter):
+            return False
     
-    return hand_letters
+    return True
 
+    
+    #### second solution:
 
+    # word_dict = dict(Counter(word.upper()))
+    # letter_bank_dict = dict(Counter(letter_bank))
+    # for k in word_dict:
+    #     if k not in letter_bank_dict or word_dict[k] > letter_bank_dict[k]:
+    #         return False
+    # return True
 
-def uses_available_letters(word, letter_bank):
-    pass
+            
 
+            
 def score_word(word):
     pass
+    
+
 
 def get_highest_word_score(word_list):
-    pass
+   pass
+                
+ 
+                
+            
+            
 
-print(draw_letters())
+
+
+    
+    
+
