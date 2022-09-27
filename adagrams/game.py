d777ea55a95009f8587fb1c18af1a2c9e9f55a92
Miranda <miranda@Jays-MacBook-Pro.local>
2022-09-27 13:51:39 -0700

    passed wave 2 with version 1 of conditional statement

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 # import string
 
 
@@ -13,7 +14,7 @@ LETTER_POOL = {
     'Y': 2, 'Z': 1
 }
 
-
+###### wave 1 ########
 def build_letter_pool_list():
     letter_pool_list = []
     for key, value in LETTER_POOL.items():
@@ -32,21 +33,50 @@ def draw_letters():
     return letter
     
 
-
-
-
-
-# print(draw_letters(random_letter))
     
-
-
-
-
-
-
+###### wave 2 ########
 
 def uses_available_letters(word, letter_bank):
-    pass
+
+    '''
+- Has two parameters:
+   - `word`, the first parameter, describes some input word, and is a string
+   - `letter_bank`, the second parameter, describes an array of drawn letters in a hand. 
+      You can expect this to be an array of ten strings, with each string representing a letter
+- Returns either `True` or `False`
+- Returns `True` if every letter in the `input` word is available 
+  (in the right quantities) in the `letter_bank`
+- Returns `False` if not; if there is a letter in `input` that is not 
+present in the `letter_bank` or has too much of compared to the `letter_bank`
+    '''
+    word = word.upper()
+
+    letter_bank_dict = {element: letter_bank.count(element) for element in letter_bank}
+    word_dict = {element: word.count(element) for element in word}
+
+    for letter,num in word_dict.items():
+        if letter not in letter_bank_dict.keys():
+            return False
+        elif letter in letter_bank_dict:
+            letter_bank_dict[letter] -= num
+            if letter_bank_dict[letter] < 0:
+                return False
+        # else:
+        #     return False
+        
+        
+    return True
+
+            
+
+
+
+
+    # print(letter_bank_dict)
+    print(word_dict)
+
+
+
 
 def score_word(word):
     pass
