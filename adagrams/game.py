ea7cf1c12edd8e1e4f4d8b6f0665b7004a2cd2b4
Isabella <isabella.vanstedum@gmail.com>
2022-09-26 23:44:54 -0300

    Completed Wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -77,7 +77,24 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    point_system = {
+        ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") : 1,
+        ("D","G"): 2,
+        ("B", "C", "M", "P"):3,
+        ("F", "H", "V", "W", "Y"):4,
+        ("K"): 5,
+        ("J","X"):8,
+        ("Q","Z"):10
+    }
+    word = word.upper()
+    score = 0
+    for letter in word:
+        for key, point in point_system.items():
+            if letter in key:
+                score += point
+    if 6 < len(word) <= 10:
+        score+=8
+    return score
 
 def get_highest_word_score(word_list):
     pass
