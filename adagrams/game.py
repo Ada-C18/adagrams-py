8eb9c0d726dc58a13918ba3784aff6f0364b15e5
Maggie <mjtice0@gmail.com>
2022-09-27 17:15:06 -0600

    first commit for Adagrams project for Maggie

--- adagrams/game.py
+++ adagrams/game.py
@@ -5,10 +5,23 @@ def draw_letters():
     pass
 
 def uses_available_letters(word, letter_bank):
+
+    #valid_input = True (valid word)
+    #invalid_input = False (invalid word)
+    #loop through and check if a word is valid, return True
+
+    #loop through and check if a word is invalid, return false
+    
+    
     pass
 
 def score_word(word):
+   
     pass
 
 def get_highest_word_score(word_list):
+    #word_list = previous list from function 1,2
+    #list_of_scores = [] list to store scores
+    #sum = sum of scores/calculation
+    #highest_score = the highest score in the world_list
     pass
