d9b41f6004c6635f8fe8c4490203e27a3141ee22
Anaiq <>
2022-09-27 14:34:15 -0500

    added docstring

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,3 +1,20 @@
+##-----------------------------------------##
+##-----------------WAVE 1------------------##
+##-----------------------------------------##
+"""
+1. Your first task is to build a hand of 10 letters for the user. 
+-No parameters
+-Returns an array of ten strings
+    -Each string should contain exactly one letter
+    -These represent the hand of letters that the player has drawn
+-The letters should be randomly drawn from a pool of letters
+    -This letter pool should reflect the distribution of letters as described in the table below
+    -There are only 2 available C letters, so draw_letters cannot ever return more than 2 Cs
+    -Since there are 12 Es but only 1 Z, it should be 12 times as likely for the user to draw an E as a Z
+-Invoking this function should not change the pool of letters
+    -Imagine that the user returns their hand to the pool before drawing new letters
+    reference: https://theprogrammingexpert.com/get-random-value-from-dictionary-python/
+"""
 import random
 
 def draw_letters():
