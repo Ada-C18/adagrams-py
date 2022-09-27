e97ecc17fd4b8b67b8cd451c7b9891bf5648a157
Tia Willis <tiawillis1@icloud.com>
2022-09-26 21:37:10 -0600

    Wave 1 completed by Shelby and Neema functions made draw_letters, all tests passed.

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,53 @@
+import random  
 
-
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 def draw_letters():
-    pass
+
+    num_letters = []
+    random_hands = []
+
+    for letter in LETTER_POOL:
+        if letter not in num_letters:
+            num_letters = list(LETTER_POOL.keys())
+            
+    for i in range(0, len(num_letters)):
+        num_letters.extend(num_letters[i] * LETTER_POOL[num_letters[i]])
+    del num_letters[:26]
+
+    # for i in range(0,10):
+    #     if len(random_hands) < 10:
+    #         random_letter = random.choice(num_letters)
+    #         random_hands.append(random_letter)
+    
+    return (random.sample(num_letters,10))
+
 
 def uses_available_letters(word, letter_bank):
     pass
