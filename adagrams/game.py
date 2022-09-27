6730eb554e321f3fa0dd73c18b7619c8f3b30224
Kate <varindakate@gmail.com>
2022-09-27 14:01:08 -0700

    wave 2. passed 2 tests. failed 3.

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,12 @@
 import copy
 import random
+    # create a list (ORIGINAL_POOL) with all the given letters
+    #LOOP:
+        # create deep copy of ORIGINAL_POOL (GAME_POOL)
+        # use random.choice(list) function to draw from pool (GAME_POOL) of letters
+        # make another list for player's hand (HAND)
+        # once letter is picked remove letter from pool list
+        # use while loop max the letters of player's hand to 10
 
 def draw_letters():
     ORIGINAL_POOL = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C",
@@ -20,55 +27,32 @@ def draw_letters():
         print(hand)
     return hand
 
-draw_letters()
+    # upper_word = word.upper()
+    # A = True
 
-# ORIGINAL_POOL = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C",
-# "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-# "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", 
-# "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N",
-# "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", 
-# "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", 
-# "V", "V", "W", "W", "X", "Y", "Y", "Z"]
-
-
-# print({ORIGINAL_POOL.count("A")})
-# print({ORIGINAL_POOL.count("B")})
-# print({ORIGINAL_POOL.count("C")})
-# print({ORIGINAL_POOL.count("D")})
-# print({ORIGINAL_POOL.count("E")})
-# print({ORIGINAL_POOL.count("F")})
-# print({ORIGINAL_POOL.count("G")})
-# print({ORIGINAL_POOL.count("H")})
-# print({ORIGINAL_POOL.count("I")})
-# print({ORIGINAL_POOL.count("J")})
-# print({ORIGINAL_POOL.count("K")})
-# print({ORIGINAL_POOL.count("L")})
-# print({ORIGINAL_POOL.count("M")})
-# print({ORIGINAL_POOL.count("N")})
-# print({ORIGINAL_POOL.count("O")})
-# print({ORIGINAL_POOL.count("P")})
-# print({ORIGINAL_POOL.count("Q")})
-# print({ORIGINAL_POOL.count("R")})
-# print({ORIGINAL_POOL.count("S")})
-# print({ORIGINAL_POOL.count("T")})
-# print({ORIGINAL_POOL.count("U")})
-# print({ORIGINAL_POOL.count("V")})
-# print({ORIGINAL_POOL.count("W")})
-# print({ORIGINAL_POOL.count("X")})
-# print({ORIGINAL_POOL.count("Y")})
-# print({ORIGINAL_POOL.count("Z")})
+    # for letter in upper_word:
+    #     if letter in letter_bank:
+    #         letter_bank.remove(letter)
+    #         A = True
+    #     else:
+    #         A = None
+    # return A
 
 
-    # create a list (ORIGINAL_POOL) with all the given letters
-    #LOOP:
-        # create deep copy of ORIGINAL_POOL (GAME_POOL)
-        # use random.choice(list) function to draw from pool (GAME_POOL) of letters
-        # make another list for player's hand (HAND)
-        # once letter is picked remove letter from pool list
-        # use while loop max the letters of player's hand to 10
+
+
+
 
 def uses_available_letters(word, letter_bank):
-    pass
+    upper_word = word.upper()
+    letter_list = []
+
+    for letter in upper_word:
+        letter_list.append(letter)
+    if letter_list in letter_bank:
+        return True
+    else:
+        return False
 
 def score_word(word):
     pass
