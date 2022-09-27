e855d73ecc27b53308145f9573457cfce46e31bf
Maxwell <maxpasqualis@gmail.com>
2022-09-27 13:36:19 -0700

    Added dictionary to determine letter frequency

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,18 @@
 def draw_letters():
-    pass
+    letter_pool = []
+    letter_pool_amounts = {
+        'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12,
+        'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1,
+        'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8,
+        'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6,
+        'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+    }
+
+    # iterate through letter_pool_amounts keys and values
+    
+    # for each key, add it [value] number of times to letter_pool list
+
+    # use random.randint w/ index 10 times to draw 10 random letters
 
 def uses_available_letters(word, letter_bank):
     pass
