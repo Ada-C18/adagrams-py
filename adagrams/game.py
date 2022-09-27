a89cab17c41fe34c4f4b75f2df886dae4ead4d81
Nashwa <n.alsharki@gmail.com>
2022-09-27 10:17:11 -0400

    Updated wave 1 and wave 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,38 +1,32 @@
 import random
 
 letter_dict = {
-"A" : 9 , "N" : 6,
-"B" : 2, "O" : 8, 
-"C" : 2, "P" : 2,
-"D" : 4,  "Q" : 1, 
-"E" : 12, "R" : 6,
-"F": 2, "S" : 4,
-"G" : 3, "T" : 6, 
-"H" : 2, "U" : 4,
-"I" : 9, "V" : 2,
-"J" : 1, "W" : 2,
-"K" : 1, "X" : 1,
-"L" : 4, "Y" : 2,
-"M" : 2, "Z" : 1 
-}
+                "A" : 9 , "N" : 6, "B" : 2, "O" : 8, "C" : 2, "P" : 2, "D" : 4, "Q" : 1,
+                "E" : 12, "R" : 6, "F": 2, "S" : 4, "G" : 3, "T" : 6, "H" : 2, "U" : 4,
+                "I" : 9, "V" : 2, "J" : 1, "W" : 2, "K" : 1, "X" : 1, "L" : 4, "Y" : 2,
+                "M" : 2, "Z" : 1 
+                }    
 
-def draw_letters():
-    player_hand = random.choices(list(letter_dict.keys()), weights=letter_dict.values(), k=10)       
+def draw_letters():    
+    letter_pool = []
+    for letter in letter_dict:
+        letter_pool += list(letter) * letter_dict[letter]
+    
+    player_hand = []
+    while len(player_hand) < 10:
+        letter = random.choice(letter_pool) 
+        if player_hand.count(letter) < letter_pool.count(letter):
+            player_hand += letter
+    
     return player_hand
-    # come back and complete longer version
 
 def uses_available_letters(word, letter_bank):
-    letter_bank2 = letter_bank
-    counter = len(word)
-    for letter in word:
-        if letter not in letter_bank2:
+    letters = letter_bank.copy()
+    for letter in word.upper():
+        if letter not in letters:
             return False
-        else:
-            letter_bank2.remove(letter)
-            counter -= 1
-    return True
-
-            
+        letters.remove(letter)
+    return True          
 
 def score_word(word):
     pass
