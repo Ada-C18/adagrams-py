21dfc9401d8c746d7bfdca04230b81727acb32cd
Makeesha Pruitt <makeeshalp@gmail.com>
2022-09-26 20:44:07 -0400

    resolving merge error on wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,19 +1,15 @@
-def draw_letters():
-# Create empty list
-# Create empty counter dictionary
-# Use for loop i in range(10)
-# Use a built in funciton to draw random letter random.choice()
-# 
-# Use if statement to check if random letter is not drawn more 
-# than dict value comparing values from counter dict
-# Append to empty list
-# Return list of 10 letters(strings)
 
+import random
+
+def draw_letters():
+    pass
+  
 def uses_available_letters(word, letter_bank):
     pass
-
+    
 def score_word(word):
     pass
 
 def get_highest_word_score(word_list):
     pass
+
