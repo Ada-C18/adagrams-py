3fd7ebbd3e9eed2dbd8b754311628c87ea2e48c3
Isabella <isabella.vanstedum@gmail.com>
2022-09-27 17:26:17 -0300

    refactoring code with comments

--- adagrams/game.py
+++ adagrams/game.py
@@ -28,45 +28,62 @@ LETTER_POOL20 = {
     'Y': 2, 
     'Z': 1
 }
-def random_selection():
+# def random_selection():
+#     values = list(LETTER_POOL20.values())
+#     keys = list(LETTER_POOL20.keys())
+#     percentages= []
+#     for num in values:
+#         percentages.append(num)
+#     tup = tuple(percentages)
+#     random_l = random.choices(keys , weights = tup)
+#     return ''.join(random_l)
+    
+# def check_available(random, players_hand, LETTER_POOL20):
+#     if random in LETTER_POOL20:  
+#         if LETTER_POOL20[random] == 0:     
+#             return False
+#         else:
+#             LETTER_POOL20[random] -=1   
+#             players_hand.append(random)
+#             return True
+# def draw_letters():
+#     players_hand = []            
+#     i=0
+#     if all(LETTER_POOL20.values()) == 0:
+#         return []
+#     random_selection()
+#     available = True
+#     while available and len(players_hand)<10 and i <26:
+#         letter = random_selection()
+#         check_available(letter, players_hand,LETTER_POOL20)
+#         i +=1
+#     return players_hand
+def draw_letters():
     values = list(LETTER_POOL20.values())
     keys = list(LETTER_POOL20.keys())
-    percentages= []
-    for num in values:
-        percentages.append(num)
-    tup = tuple(percentages)
-    random_l = random.choices(keys , weights = tup)
-    return ''.join(random_l)
-    
-def check_available(random, players_hand, LETTER_POOL20):
-    if random in LETTER_POOL20:  
-        if LETTER_POOL20[random] == 0:     
-            return False
-        else:
-            LETTER_POOL20[random] -=1   
-            players_hand.append(random)
-            return True
-def draw_letters():
-    players_hand = []            
-    i=0
-    if all(LETTER_POOL20.values()) == 0:
-        return []
-    random_selection()
-    available = True
-    while available and len(players_hand)<10 and i <26:
-        letter = random_selection()
-        check_available(letter, players_hand,LETTER_POOL20)
-        i +=1
+    players_hand =random.sample(keys, counts = values, k =10)
     return players_hand
-    
-
+# def uses_available_letters(word, letter_bank):
+#     list_bool = []
+#     word = word.upper()
+#     amount_bank = Counter(letter_bank)
+#     for letter in word:
+#         if letter in letter_bank and amount_bank[letter] > 0 :
+#             amount_bank[letter] -= 1
+#             list_bool.append(True)
+#         else:
+#             list_bool.append(False)
+#     if False in list_bool:
+#         return False
+#     else:
+#         return True
 def uses_available_letters(word, letter_bank):
     list_bool = []
     word = word.upper()
-    amount_bank = Counter(letter_bank)
+    available_bank = Counter(letter_bank)
     for letter in word:
-        if letter in letter_bank and amount_bank[letter] > 0 :
-            amount_bank[letter] -= 1
+        if letter in letter_bank and available_bank[letter] > 0:
+            available_bank[letter] -= 1
             list_bool.append(True)
         else:
             list_bool.append(False)
@@ -74,7 +91,6 @@ def uses_available_letters(word, letter_bank):
         return False
     else:
         return True
-
 
 def score_word(word):
     point_system = {
