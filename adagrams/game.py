964e28e3cceb1c5502fb750f77eff9a68b0e2727
Andrea <andreagalindo.guerra@gmail.com>
2022-09-27 17:46:08 -0400

    Created function draw_letters and it passes all tests from wave_1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,17 +1,7 @@
+from collections import Counter
 import random
 
-def draw_letters():
-    # return an array of ten strings
-        # each str should contain exactly 1 letter
-        # these represent the hand of letters that the player has drawn
-    # the letters should be randomly drawn from LETTER_POOL
-        # This letter pool should reflect the distribution of letters as described in the table below
-        # There are only 2 available C letters, so draw_letters cannot ever return more than 2 Cs
-        # Since there are 12 Es but only 1 Z, it should be 12 times as likely for the user to draw an 
-        # E as a Z
-    # Invoking this function should not change the pool of letters
-        # Imagine that the user returns their hand to the pool before drawing new letters
-    LETTER_POOL = {
+LETTER_POOL = {
     'A': 9, 
     'B': 2, 
     'C': 2, 
@@ -37,27 +27,50 @@ def draw_letters():
     'W': 2, 
     'X': 1, 
     'Y': 2, 
-    'Z': 1
+    'Z': 1,
 }
-    letters = []
-    for i in range(10):
-        letters.append(random.choice(list(LETTER_POOL.keys())))
 
-    dict_letters = {}
-    for letter in letters: 
-        dict_letters[letter] = dict_letters.get(letter, 0) + 1
-        
-    for letter, counts in dict_letters.items():
-        if counts > LETTER_POOL[letter]:
-            print(f"not enough letters {letter}")
+LETTER_VALUES = {"A": 1,
+                 "B": 3,
+                 "C": 3,
+                 "D": 2,
+                 "E": 1,
+                 "F": 4,
+                 "G": 2,
+                 "H": 4,
+                 "I": 1,
+                 "J": 1,
+                 "K": 5,
+                 "L": 1,
+                 "M": 3,
+                 "N": 1,
+                 "O": 1,
+                 "P": 3,
+                 "Q": 10,
+                 "R": 1,
+                 "S": 1,
+                 "T": 1,
+                 "U": 1,
+                 "V": 4,
+                 "W": 4,
+                 "X": 8,
+                 "Y": 4,
+                 "Z": 10,
+                 "#": 0,
+}
 
-    return letters
 
-def uses_available_letters(word, letter_bank):
-    print("testing git")
+def draw_letters() -> list:
+    """
+    Returns a list with ten letters chosen randomly from LETTER_POOL
+    """
+    letter_pool_copy = LETTER_POOL.copy()
+    hand = []
+    while len(hand) < 10:
+        letter = random.choice(list(letter_pool_copy.keys()))
+        if letter_pool_copy[letter] > 0:
+            hand.append(letter)
+            letter_pool_copy[letter] =- 1
 
-def score_word(word):
-    pass
+    return hand
 
-def get_highest_word_score(word_list):
-    pass
