fa25bec3cc2d6c547de49cb485e091972673092e
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-09-27 13:27:34 -0700

    temp

--- adagrams/game.py
+++ adagrams/game.py
@@ -38,17 +38,47 @@ def draw_letters():
 
     for i in range(10):
         letter_choice = random.choice(all_letter_pool)
-    # print(letter_choice)
         letters.append(letter_choice)
         all_letter_pool.remove(letter_choice)
     return letters
 
 #wave 2
 def uses_available_letters(word, letter_bank):
-    pass
+    # create a copy of letter bank
+    copy_bank = letter_bank[:]
+    letters_used = []
+    # for each letter in word, if letter is in 
+    # letter bank, append letter to an empty list and 
+    # remove from letter bank copy
+    for letter in word.upper():
+        letters_used.append(letter)
+        if letter in copy_bank:
+            copy_bank.remove(letter)
+    # this checks that letters weren't reused 
+    if len(copy_bank) == len(letter_bank) - (len(letters_used)):
+        # this evaluates whether all letters used are in the letter bank
+        return True
+        # return all(i in letter_bank for i in letters_used)
+    return False
+
 #wave 3
 def score_word(word):
-    pass
+    # wave 3
+    d1 = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10}
+
+# dict = {1:'AEIOULNRST'}
+
+# word_list = ["AAAAAAAAAA", "BBBBBB"]
+# scores = []
+# for word in word_list:
+#     score = 0
+    score = 0 
+    for letter in word.upper():
+        score += d1[letter]
+        if len(word) >=7:
+            score += 8
+        
+    return score
 # Wave 4
 def get_highest_word_score(word_list):
     pass
