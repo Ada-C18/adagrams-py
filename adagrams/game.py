6780b0f9b68d9d530a0c34baac1f4c0efa1e6b89
Erika Torres Figueroa <torrespurple@gmail.com>
2022-09-27 12:54:03 -0400

    finished score_word function in wave 3. will need to retest with new tests.

--- adagrams/game.py
+++ adagrams/game.py
@@ -61,7 +61,47 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+
+    word_dict = {
+        "A" : 1, 
+        "E" : 1, 
+        "I" : 1, 
+        "O" : 1, 
+        "U" : 1, 
+        "L" : 1, 
+        "N" : 1, 
+        "R" : 1, 
+        "S" : 1, 
+        "T" : 1, 
+        "D" : 2, 
+        "G" : 2, 
+        "B" : 3, 
+        "C" : 3, 
+        "M" : 3, 
+        "P" : 3, 
+        "F" : 4, 
+        "H" : 4, 
+        "V" : 4, 
+        "W" : 4, 
+        "Y" : 4, 
+        "K" : 5, 
+        "J" : 8, 
+        "X" : 8, 
+        "Q" : 10, 
+        "Z" : 10
+    }
+    points = 0
+    new_word = word.upper()
+
+    for letter in new_word:
+        if letter.isalpha():
+            points += word_dict[letter]
+        else:
+            points += 0
+    if len(new_word) == 7 or 8 or 9 or 10:
+        points += 8
+    return points
+
 
 def get_highest_word_score(word_list):
     pass 
