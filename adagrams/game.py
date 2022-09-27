71dd69e5f5dd5c513d77f204bcb838b0b1074825
nina-tuyen tran <ninatuyentran@gmail.com>
2022-09-27 13:41:42 -0700

    updated wave1 to complete all tests and account for letter removal when selected

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,44 +2,49 @@ import random
 def draw_letters():
     allletters = []
     alphabet = {
-        "A": 9,
-        "B": 2,
-        "C": 2,
-        "D": 4,
-        "E": 12,
-        "F": 2,
-        "G": 3,
-        "H": 2,
-        "I": 9,
-        "J": 1,
-        "K": 1,
-        "L": 4,
-        "M": 2, 
-        "N": 6,
-        "O": 8,
-        "P": 2,
-        "Q": 1,
-        "R": 6,
-        "S": 4,
-        "T": 6,
-        "U": 4,
-        "V": 2,
-        "W": 2,
-        "X": 1,
-        "Y": 2,
-        "Z": 1
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
     }
+    #expanded dictionary per numbers of letters
     for letter, number in alphabet.items():
         for i in range(number):
             allletters.append(letter)
     
-
+    #picking 10 random letters from expanded dictionary
     gameletters = []
     for letter in range(10):
         number = random.randint(0, 27)
         random_letter = allletters[number]
         gameletters.append(random_letter)
+        allletters.remove(random_letter)
+        
     return gameletters
+
+
 
 def uses_available_letters(word, letter_bank):
     new_list = []
