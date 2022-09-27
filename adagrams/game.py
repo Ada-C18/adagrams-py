9e070d9ab141448fc627616c8d54a4f0031a12c5
Annie <annie.gallagher7@gmail.com>
2022-09-27 14:47:38 -0600

    updated score_word score_chart to search the dictionary keys rather than values

--- adagrams/game.py
+++ adagrams/game.py
@@ -66,14 +66,35 @@ def uses_available_letters(word, letter_bank):
 
         
 def score_word(word):
+    # change this to a constant big O rather than linear by looking up by key (changing key to the letter)
+    # because looking it up by key is constant whereas looking it up by value and returning the key linear
     score_chart = {
-        1:["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-        2:["D", "G"],
-        3: ["B", "C", "M", "P"],
-        4: ["F", "H", "V", "W", "Y"],
-        5: ["K"],
-        8: ["J", "X"],
-        10: ["Q", "Z"]
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
     }
 
     word = word.upper()
@@ -83,9 +104,9 @@ def score_word(word):
         return score
     
     for letter in word:
-        for key, value in score_chart.items():
-            if letter in value:
-                score += key
+        for key in score_chart.keys():
+            if letter == key:
+                score += score_chart[key]
     if len(word)>= 7:
         score += 8
     return score
