59a85475d8f955189b9346028de0036774c88d3c
Dominique <dominiquedaba@gmail.com>
2022-09-27 13:55:49 -0700

    passed Wave 3 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -27,15 +27,15 @@ def draw_letters():
 	"Y" : 2,
 	"Z" : 1}
     # make a copy of the dictionary so that original dictionary doesn't change
-    pool_of_letters_copy = POOL_OF_LETTERS.copy()
+    pool_of_letters_copy : POOL_OF_LETTERS.copy()
     # taking just the keys of the dictionary (all the letters)
-    just_letters = list(pool_of_letters_copy)
+    just_letters : list(pool_of_letters_copy)
     # create an empty list to store the hand
     hand_list = []
     # length of the hand
     while len(hand_list) < 10:
         # use .choice to randomly select letters
-        hand = random.choice(just_letters)
+        hand : random.choice(just_letters)
         # if the letter has a value more than 0 in the dictionary copy, append to the empty list
         # and decrement the value by 1 
         if pool_of_letters_copy[hand] >= 1:
@@ -45,9 +45,9 @@ def draw_letters():
 
 def uses_available_letters(word, letter_bank):
     # copy the letter bank so that original letter bank doesn't change
-    letter_bank_copy = letter_bank.copy()
+    letter_bank_copy : letter_bank.copy()
     # for each letter in the word (it should be uppercase so use ".upper")
-    # if the letter is in the letter_bank_copy list, remove it from the lette_bank_copy
+    # if the letter is in the letter_bank_copy list, remove it from the letter_bank_copy
     # after checking all the letters in the word, if all the letters in the word are in the letter_bank_copy
     # return "True"
     for letter in word.upper():
@@ -58,7 +58,39 @@ def uses_available_letters(word, letter_bank):
     return True
     
 def score_word(word):
-    pass
+    letter_points = {
+    "A": 1,
+    "E" : 1, 
+    "I" : 1,
+    "O" : 1,
+    "L" : 1,
+    "N" : 1,
+    "R" : 1,
+    "S" : 1,
+    "T" : 1,
+    "D" : 2, 
+    "G" : 2,
+    "B" : 3,
+    "C" : 3,
+    "M" : 3,
+    "P" : 3,
+    "F" : 4,
+    "H" : 4,
+    "V" : 4,
+    "W" : 4,
+    "Y" : 4,
+    "K" : 5,
+    "J" : 8,
+    "X" : 8,
+    "Q" : 10,
+    "Z" : 10,
+    }
+    score = 0
+    for letter in word:
+        score += letter_points[letter.upper()]
+    if len(word) >= 7:
+        score += 8
+    return score
 
 def get_highest_word_score(word_list):
     pass
