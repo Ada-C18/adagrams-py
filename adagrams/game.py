b98aaa5cce9cdf03579ce212d4fbdd6d50fb5aac
Erika Torres Figueroa <torrespurple@gmail.com>
2022-09-26 20:32:09 -0400

    finished uses_available_letters function for wave 2.

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,7 +2,21 @@ def draw_letters():
     pass
 
 def uses_available_letters(word, letter_bank):
-    pass
+ 
+    new_word = word.upper()
+    copy_bank = letter_bank.copy()
+    
+    for letter in new_word:
+        
+      if letter in copy_bank:
+        copy_bank.remove(letter)
+      else: 
+        return False
+    return True
+
+
+
+
 
 def score_word(word):
     pass
