b8a0ef70612120b99304cfb4153f4d83106b0c66
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-09-27 14:06:30 -0700

    completed score_word function

--- adagrams/game.py
+++ adagrams/game.py
@@ -52,7 +52,41 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    word = word.upper()
+    score_chart_dict = {
+        "A": 1,
+        "B": 3,
+        "C": 3,
+        "D": 2, 
+        "E": 1,
+        "F": 4,
+        "G": 2,
+        "H": 4,
+        "I": 1,
+        "J": 8,
+        "K": 5,
+        "L": 1,
+        "M": 3,
+        "N": 1,
+        "O": 1,
+        "P": 3,
+        "Q": 10,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "U": 1,
+        "V": 4,
+        "W": 4,
+        "X": 8,
+        "Y": 4,
+        "Z": 10,
+    }
+    sum = 0
+    if len(word) >= 7:
+        sum += 8
+    for letter in word:
+        sum += score_chart_dict[letter]
+    return sum
 
 def get_highest_word_score(word_list):
     pass
