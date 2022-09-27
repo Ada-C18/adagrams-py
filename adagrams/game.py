22c668d37e1cd4f5ee240b06d28a1584fc18f129
Kae Sluder <kaesluder@fastmail.com>
2022-09-26 20:23:02 -0400

    passing version of wave 1 using random.sample

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,57 @@
+from random import sample
+
+
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    # use random.sample to draw 10 letters from the 
+    # letter bank.
+
+    # get the list of letter and count for each letter
+    # as a list 
+    letter_list = LETTER_POOL.keys()
+    letter_count = LETTER_POOL.values()
+
+    # sample draws k items from the list using counts 
+    # to determine the frequency of each item. 
+    # https://docs.python.org/3/library/random.html#random.sample
+    # requires python version 3.9 or greater 
+    this_hand = sample(letter_list, k=10, counts=letter_count)
+
+    # print('-----------------')
+    # print(this_hand)
+    # print('-----------------')
+
+
+    return this_hand
 
 def uses_available_letters(word, letter_bank):
     pass
