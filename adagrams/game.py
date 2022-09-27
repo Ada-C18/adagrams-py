80d2a8f2ab35121a749b996ddbd0cd36460df645
Neida <neida@duck.com>
2022-09-26 21:08:30 -0400

    added space

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-def draw_letters(): 
-    pass
-=======
 def draw_letters():
     '''
     
@@ -17,7 +13,6 @@ def draw_letters():
     # return list 
 
     letter_pool = [{"A" : 9}, {"N" : 6}, {"B" : 2}, {"O" : 8}, {"C" : 2}, {"P" : 2}, {"D" : 4}, {"Q" : 1}, {"E" : 12}, {"R" : 6}, {"F" : 2}, {"S" : 4}, {"G": 3}, {"T" : 6}, {"H" : 2}, {"U" : 4}, {"I" : 9},  {"V" : 2}, {"J" : 1}, {"W" : 2}, {"K" : 1}, {"X" : 1}, {"L" : 4}, {"Y" : 2}, {"M" : 2}, {"Z" : 1}]
->>>>>>> c0d4a2c6134c9cc98dd3d7b5e8f3be8ce1c6b844
 
 def uses_available_letters(word, letter_bank):
     pass
