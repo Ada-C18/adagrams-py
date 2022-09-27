f7ab49bbc0a6f494f0fe0b445f47fc1016698e17
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-09-27 13:14:16 -0700

    created function draw_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,45 @@
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    letter_pool_list = []
+    for letter, number in LETTER_POOL.items():
+        letter_pool_list.extend([letter]*number)
+    hand_list = []
+    while len(hand_list) < 10:
+        random.shuffle(letter_pool_list)
+        hand_list.append(letter_pool_list.pop())
+    print("letter pool", letter_pool_list)
+    print("return list", hand_list)
+    return hand_list
 
 def uses_available_letters(word, letter_bank):
     pass
