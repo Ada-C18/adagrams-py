5254f35c81811e60cae79f4099817b11442de263
Alaere Nekekpemi <alaeren00@gmail.com>
2022-09-26 22:09:15 -0700

    Changes made to wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,21 +1,43 @@
 import random
-
-letter_pool = [['A'] * 9, ['B'] * 2, ['C'] * 2, ['D'] * 4, ['E'] * 12, ['F'] * 2, ['G'] * 3, ['H'] * 2, ['I'] * 9, 'J', 'K' , ['L'] * 4, 
-['M'] * 2, ['N'] * 6, ['O'] * 8, ['P'] * 2, 'Q', ['R'] * 6, ['S'] * 4, ['T'] * 6, ['U'] * 4, ['V'] * 2, ['W'] * 2, 'X', ['Y'] * 2, 'Z']
-# Nested list that contains all the letters that a player can draw from
+from itertools import chain
+letter_bank = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
 def draw_letters():
-    
-    flat_pool = [l for letter in letter_pool for l in letter]
-    # flattened list from letter_pool
+    pool = list(chain(*[random.choices(key, k = val) for key, val in letter_bank.items()]))
     hand = []
-
-    for i in range(10):
-        letter = random.choice(flat_pool) # function that chooses a random letter from the flattened list 
-        hand.append(letter)
-        flat_pool.remove(letter) # after appending each letter remove that letter from letter pool
-    return hand
     
+    for i in range(10):
+        letter = random.choice(pool)
+        hand.append(letter)
+        pool.remove(letter)
+    return hand 
 def uses_available_letters(word, letter_bank):
     pass
 
@@ -29,9 +51,7 @@ def score_word(word):
         8: 'JX',
         10: 'QZ'
     }
-
     score = 0
-
     if len(word) >= 7:
         score += 8
 
@@ -40,7 +60,6 @@ def score_word(word):
             if char in letters:
                 score += point
     return score
-
 
 def get_highest_word_score(word_list):
     pass
