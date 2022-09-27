b6f3a5cdbdf2e02bbd524a9d42cf428a2488cac5
ashanti <ammanier2@gmail.com>
2022-09-26 21:27:45 -0400

    changed all tests for wave 3, all tests pass

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -17,18 +17,34 @@ def test_score_word_accurate():
     assert score3 == 17
 
 def test_score_word_accurate_ignores_case():
+    #Act
+    word1 = "a"
+    word2 = "dog"
+    word3 = "wHiMsY"
+
+    #Arrange
+    score1 = score_word(word1)
+    score2 = score_word(word2)
+    score3 = score_word(word3)
     # Assert
-    assert score_word("a") == 1
-    assert score_word("dog") == 5
-    assert score_word("wHiMsY") == 17
+    assert score1 == 1
+    assert score2 == 5
+    assert score3 == 17
 
 def test_score_zero_for_empty():
+    #Act/Arrange
+    score = score_word("")
     # Assert
-    assert score_word("") == 0
+    assert score == 0
 
 def test_score_extra_points_for_seven_or_longer():
-    # Assert
-    assert score_word("XXXXXXX") == 64
-    assert score_word("XXXXXXXX") == 72
-    assert score_word("XXXXXXXXX") == 80
+    # Arrange/Act
+    score1 = score_word("XXXXXXX") 
+    score2 = score_word("XXXXXXXX") 
+    score3 = score_word("XXXXXXXXX") 
+
+    #Assert
+    assert score1 == 64
+    assert score2 == 72
+    assert score3 == 80
     
