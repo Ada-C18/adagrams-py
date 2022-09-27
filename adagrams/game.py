aadc3d32e353d61023c7994bfcf0f23c5d9e2667
masinakh <nakhjirimasi@gmail.com>
2022-09-27 13:11:30 -0700

    wave 2 is done

--- adagrams/game.py
+++ adagrams/game.py
@@ -37,8 +37,14 @@ def draw_letters():
     return hand_list
 
 def uses_available_letters(word, letter_bank):
-    pass
-
+    letter_bank_copy = letter_bank.copy()
+    for letter in word.upper():
+        if letter in letter_bank_copy:
+            letter_bank_copy.remove(letter)
+        else:
+            return False    
+    return True
+    
 def score_word(word):
     pass
 
