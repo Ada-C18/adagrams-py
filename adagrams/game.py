c101c91e43d16bb1cd418a9dbdff6e87e2318d6f
Erika Sha <erikaasha05@gmail.com>
2022-09-26 21:45:18 -0400

    created uses_available_letters function to check if the input word uses characters in the letter_bank list or if uses too many of one character. passed tests 1-5 in test wave 2.

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,7 +2,16 @@ def draw_letters():
     pass
 
 def uses_available_letters(word, letter_bank):
-    pass
+    upper_case_word = word.upper()
+    input_letter_bank = letter_bank.copy()
+
+    for character in upper_case_word:
+        if character not in input_letter_bank:
+            return False
+        elif character in input_letter_bank:
+            input_letter_bank.remove(character)
+
+    return True
 
 def score_word(word):
     pass
