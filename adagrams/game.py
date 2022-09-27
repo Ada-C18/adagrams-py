4baf24018db466b7eac240621d3373985fcf9309
Steffanie <lopezsteffanie8@gmail.com>
2022-09-26 18:09:41 -0700

    list comprehension for var highest_dict in get_highest_word_score function

--- adagrams/game.py
+++ adagrams/game.py
@@ -80,10 +80,8 @@ def get_highest_word_score(word_list):
         score = score_word(word)
         word_score_dict[word] = score
     highest_score = max(word_score_dict.values())
-    highest_dict = {}
-    for word in word_score_dict:
-        if word_score_dict[word] == highest_score:
-            highest_dict[word] = word_score_dict[word]
+    highest_dict = {word: word_score_dict[word] for word, value in word_score_dict.items() if value == highest_score}
+
     min_len_word = min(list(highest_dict.keys()), key=len)
     for k, v in highest_dict.items():
         if len(k) == 10:
