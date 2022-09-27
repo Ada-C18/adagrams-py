dcc4e45d5d1af04522c3a8186c03cd67888d3295
Soleil <soleilherring@gmail.com>
2022-09-27 16:43:54 -0700

    made wave 2 function more readable

--- adagrams/game.py
+++ adagrams/game.py
@@ -54,15 +54,13 @@ def draw_letters():
 def uses_available_letters(word, letter_bank):
     new_list = letter_bank.copy()
     word_upper = word.upper()
-    try:
-        for i in range(len(word)):
-            if word_upper[i] in new_list:
-                new_list.remove(word_upper[i])
-            else:
-                return False
-        return True
-    except KeyError:
-        return False
+
+    for i in range(len(word)):
+        if word_upper[i] in new_list:
+            new_list.remove(word_upper[i])
+        else: 
+            return False 
+    return True 
 
 def score_word(word):
     score = 0
