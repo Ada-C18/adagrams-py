b374e4e348bd001addbc5c3eda364d784e9df0ae
hang tran <hangmtran101@gmail.com>
2022-09-27 14:18:40 -0700

    wave 3 done

--- adagrams/game.py
+++ adagrams/game.py
@@ -52,19 +52,61 @@ def uses_available_letters(word, letter_bank):
             return False
 
     return True
-    # for i in word:
-    #     if i in letter_bank:
-    #         letter_bank.remove(i)
-    #     else:
-            # return False
+
     
-        
         
         
 
 
 def score_word(word):
-    pass
+
+# we have a dictionary of letters corresponding to point values. 
+
+
+
+
+    score_chart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+    score = 0
+    for letter in word.upper():
+        if letter in score_chart:
+            score += score_chart.get(letter) #get method returns the value of specified key, values() would return a list of values
+    if len(word) >= 7:
+        score += 8
+    return score
+
+
+
+
+
+
+
 
 def get_highest_word_score(word_list):
     pass
