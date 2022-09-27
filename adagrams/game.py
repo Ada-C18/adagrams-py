ec17b11043f542b30af95a3680c6fc9799a8e68e
Ariel <gabriellaw15@gmail.com>
2022-09-27 06:53:17 -0700

    Deleted space typo by O in pool dict in function draw_letters(). Function passes wave 1 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,8 +2,6 @@ import random
 import string
 
 # WAVE 1
-
-
 def draw_letters():
     # 1. Create empty list variable
     hand = []
@@ -23,7 +21,7 @@ def draw_letters():
         "L": 4,
         "M": 2,
         "N": 6,
-        "O ": 8,
+        "O": 8,
         "P": 2,
         "Q": 1,
         "R": 6,
@@ -40,21 +38,13 @@ def draw_letters():
     # 3. Use loop to keep track of # of letters in hand
     while len(hand) < 10:
 
-        # Randomly select a letter
+        # 4. Randomly select a letter
         random_letter = random.choice(string.ascii_uppercase)
-        # print(f"Random letter: {random_letter}")
 
         if random_letter in hand:
-            # POTENTIALLY: add dictionary with letters in hand and adding the count to the value as we go
             letter_count = hand.count(random_letter)
-            # print(f"Hand letter qty: {letter_count}")
-
-            # print(f"Pool[random_letter]: {pool[random_letter]}.")
-            # print(f"Hand: {hand}")
-
             # Check pool letter count
             # If letter quantity less than number of letters in hand
-            # FIX LOGIC HERE
             if letter_count < pool[random_letter]:
                 # append letter to list
                 hand.append(random_letter)
@@ -65,17 +55,9 @@ def draw_letters():
 
         # If not, pick a new letter
 
-        # 5. Return list of lists
-    # print(hand)
+    # 5. Return list of lists
+
     return hand
-
-
-# print(draw_letters())
-
-# print(random.choice(string.ascii_uppercase))
-
-# mylist = ["apple", "banana", "cherry"]
-# print(random.choices(mylist, weights = [10, 1, 1], k = 14))
 
 # WAVE 2
 
