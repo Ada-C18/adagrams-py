0e3cae77ffba6bf4787232d4c0b79b12b96188ef
Anjula Tamang <parkhrinj@gmail.com>
2022-09-27 12:56:59 -0400

    Test 3 passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -48,7 +48,42 @@ def uses_available_letters(word, letter_bank):
     
 
 def score_word(word):
-    pass
+    letter_score = {
+        "A" : 1, 	"N" : 1,
+        "B" : 3, 	"O" : 1,
+        "C" : 3, 	"P" : 3,
+        "D" : 2, 	"Q" : 10,
+        "E" : 1,	"R" : 1,
+        "F" : 4, 	"S" : 1,
+        "G" : 2, 	"T" : 1,
+        "H" : 4, 	"U" : 1,
+        "I" : 1, 	"V" : 4,
+        "J" : 8, 	"W" : 4,
+        "K" : 5, 	"X" : 8,
+        "L" : 1, 	"Y" : 4,
+        "M" : 3, 	"Z" : 10, 
+    }
+    total = 0
+    for letter in word:
+        letter = letter.upper()
+        total += letter_score[letter]
+        
+    if len(word) == 0:
+            return total
+    elif len(word) >= 7 and len(word) <= 10:
+        total += 8
+        return total
+    return total
+   
+        
+    
 
 def get_highest_word_score(word_list):
-    pass
+    words_with_scores = []
+    highest_score = 0
+    for word in word_list:
+        if score_word(word) > highest_score:
+            highest_score = score_word(word)
+            words_with_scores.append([word,highest_score])
+    return ([word,highest_score])
+    
