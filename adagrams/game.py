47818e042f9a0bb17d8974e15324ca75d0d32b29
Geiselle Holt <geiselleholt@hotmail.com>
2022-09-28 06:09:33 -0600

    Wave 3 all tests passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -67,7 +67,42 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    LETTERS_VALUES = {
+        "A" : 1,
+        "E" : 1,
+        "I" : 1,
+        "O" : 1,
+        "U" : 1,
+        "L" : 1,
+        "N" : 1,
+        "R" : 1,
+        "S" : 1,
+        "T" : 1,
+        "D" : 2,
+        "G" : 2,
+        "B" : 3,
+        "C" : 3,
+        "M" : 3,
+        "P" : 3,
+        "F" : 4,
+        "H" : 4,
+        "V" : 4,
+        "W" : 4,
+        "Y" : 4,
+        "K" : 5,
+        "J" : 8,
+        "X" : 8,
+        "Q" : 10,
+        "Z" : 10,
+    }
+    word = word.upper()
+    word_value = 0
+    for letter in word:
+        if letter in LETTERS_VALUES:
+            word_value += LETTERS_VALUES[letter]
+    if len(word) > 6:
+        word_value += 8
+    return word_value
 
 def get_highest_word_score(word_list):
     pass
