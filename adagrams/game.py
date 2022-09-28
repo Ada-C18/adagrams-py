065c19429e3c87c27cfa3fe14172bfb39aaa139d
Reyna Diaz <reynamdiazz@gmail.com>
2022-09-28 10:13:07 -0400

    "adds draw_letters(), test_wave_01 passing"

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,19 @@
 def draw_letters():
-    pass
+    LETTER_POOL = {
+    'A': 9, 'B': 2, 'C': 2,'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 
+    'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2,  'Q': 1, 'R': 6, 
+    'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+}
+
+    letters_not_drawn = list(LETTER_POOL.keys())
+    letters_drawn = []
+    letter_count = 0
+
+    for letter in letters_not_drawn:
+        if letter not in letters_drawn and len(letters_drawn) < 10:
+            letters_drawn.append(letter)
+            letter_count += 1
+    return letters_drawn
 
 def uses_available_letters(word, letter_bank):
     pass
