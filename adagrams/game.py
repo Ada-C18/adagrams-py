b9ba37f7ca4b8d270068a59f5259feddd27f7764
Samiya <samiyai96@gmail.com>
2022-09-28 15:23:22 -0400

    WE DID IT WAVE 3.

--- adagrams/game.py
+++ adagrams/game.py
@@ -78,11 +78,41 @@ def uses_available_letters(word, letter_bank):
         return True
 
 
-
-
-
 def score_word(word):
-    pass
+    letter_value={'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10,}
+
+    total_sum=0
+    wordy=word.upper()
+    if len(word) > 6:
+        total_sum=8
+    for letter in wordy:
+        total_sum+=letter_value[letter]
+    return total_sum
 
 def get_highest_word_score(word_list):
     pass
