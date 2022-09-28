18dc72f583f7d62e0efd3e2e69f810083f4f2aa0
Kallie Gannon <gannonkallie@gmail.com>
2022-09-28 18:16:16 -0400

    func_2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,13 +1,27 @@
+import copy
 def draw_letters():
     pass 
 
 def uses_available_letters(word, letter_bank):
-    pass
+    correct_letters = []
+    new_letter_bank = copy.deepcopy(letter_bank)
+    word_case = word.upper() 
+    for char in word_case:
+        if char in new_letter_bank:
+            correct_letters.append(char)
+            new_letter_bank.remove(char)
+        if len(word_case) == len(correct_letters):
+
+            return True 
+    else:
+        return False
+
+
+    
+    
 
 def score_word(word):
     pass
 
 def get_highest_word_score(word_list):
     pass
-
-#this is a message
