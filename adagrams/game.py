35013cb0ee14325cd8bf89e71d958b7429d483a0
Caitlyn <caitlyn.homet@gmail.com>
2022-09-28 13:27:54 -0400

    added comments to help readability, started brainstorming for refactoring

--- adagrams/game.py
+++ adagrams/game.py
@@ -43,14 +43,17 @@ def draw_letters():
 
 
 def uses_available_letters(word, letter_bank):
-    available_letters = {}
 
+    # create a dictionary with available letter counts
+    available_letters = {}
     for letter in letter_bank:
         available_letters[letter] = available_letters.get(letter, 0) + 1
 
     for letter in word.upper():
+        # if letter not provided or already used up, return False
         if letter not in letter_bank or available_letters[letter] == 0:
             return False
+        # if letter still available, subtract 1 from letter count
         elif available_letters[letter] != 0:
             available_letters[letter] -= 1
 
@@ -58,6 +61,9 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     
+    # we could make one dictionary outside of the functions that holds
+    # letter distribution and letter scores
+
     score_chart = {
         1 : ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
         2 : ["D", "G"],
@@ -83,22 +89,27 @@ def get_highest_word_score(word_list):
     word_scores = {}
     top_scores = []
     
+    # build a dictionary with word scores
     for word in word_list:
         word_scores[word] = score_word(word)
     
+    # create a list of words that have the highest score
     highest_score = max(word_scores.values())
-
     for word in word_scores:
         if word_scores[word] == highest_score:
             top_scores.append(word)
     
+    # if there's one highest word, return it
     if len(top_scores) == 1:
         return top_scores[0], highest_score
+    
+    # in case of a tie...
     elif len(top_scores) > 1:
+        # return the first word with length 10
         for word in top_scores:
             if len(word) == 10:
                 return word, highest_score
-        
+        # return the first word with shortest length
         shortest_word = min(top_scores, key=len)
         return shortest_word, highest_score
 
