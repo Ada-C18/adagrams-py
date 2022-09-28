e05ed3b44a670b79d9963f5c6dfb2f5deda07f50
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-09-28 14:06:11 -0700

    Anika's attempt at wave 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -107,17 +107,20 @@ LETTER_POOL_W_VALUES = {
     }
 }
 
-
 def draw_letters():
     letters = []
     frequency_of_letters = []
     for letter, frequency in LETTER_POOL_W_VALUES.items():
         letters.append(letter)
         frequency_of_letters.append(frequency['qty'])
-    random_letters_list = random.sample(letters, counts=frequency_of_letters, k=10)
+    # random.sample returns a new list containing elements from the population
+    # while leaving the original population unchanged. counts controls the
+    # probability of an element within the sample. in this case, the greater
+    # the quantity of tiles for a letter, the higher probability that letter
+    # will be part of the random sample.
+    random_letters_list = random.sample(letters, counts =\
+        frequency_of_letters, k = 10)
     return random_letters_list
-
-
 
 def uses_available_letters(word, letter_bank):
     copy_letter_bank = letter_bank.copy()
@@ -129,9 +132,8 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    word = word.upper()
     score = 0
-    for letter in word:
+    for letter in word.upper():
         for k, v in LETTER_POOL_W_VALUES.items():
             if k == letter:
                 score += v["value"]
@@ -139,43 +141,47 @@ def score_word(word):
         score += 8
     return score
 
+# def get_highest_word_score(word_list):
+#     word_tuple_list = []
+#     highest_score = word_tuple_list[0][1]
+#     for word in word_list:
+#         score = score_word(word)
+#         length = len(word)
+#         tuple = (word, score, length)
+#         word_tuple_list.append(tuple)
+#         if tuple[1] > highest_score:
+#             highest_score = tuple[1]
 
-
-
-
-
-def get_highest_word_score(word_list):
-    word_tuple_list = []
-    highest_score = word_tuple_list[0][1]
-    for word in word_list:
-        score = score_word(word)
-        length = len(word)
-        tuple = (word, score, length)
-        word_tuple_list.append(tuple)
-        if tuple[1] > highest_score:
-            highest_score = tuple[1]
+# ****** also talk about wave 3 test stuff from slack #study-habits channel
 
 # work in progress *******
 def get_highest_word_score(word_list):
+    # need to update variable "tuple" to "word_tuple" or similar
     word_tuple_list = []
+    # we can eliminate the next two lines if we sort the word_tuple_list
+    # in such a way that the highest score is always the 0th tuple
     highest_score_tuple = ()
-
-    # highest_score = word_tuple_list[0][1]
+    highest_score = 0
 
     for word in word_list:
         score = score_word(word)
         length = len(word)
-        tuple = (word, score, length)
-        word_tuple_list.append(tuple)
-        # highest_score_tuple = tuple
-        highest_score = word_tuple_list[0][1]
-        if tuple[1] > highest_score:
-          highest_score_tuple = tuple
-          highest_score = tuple[1]
-        elif tuple[1] == highest_score:
-            if tuple[2] < highest_score_tuple[2]:
-              highest_score_tuple = tuple
-              highest_score = tuple[1]
-              
-    print(highest_score)
-    print(highest_score_tuple)
+        word_tuple = (word, score, length)
+        word_tuple_list.append(word_tuple)
+        if word_tuple[1] > highest_score:
+            highest_score_tuple = word_tuple
+            highest_score = word_tuple[1]
+        elif word_tuple[1] == highest_score:
+            if word_tuple[2] == 10 and highest_score_tuple[2] == 10:
+                break 
+            elif word_tuple[2] == 10:
+                highest_score_tuple = word_tuple
+                highest_score = word_tuple[1]
+            elif highest_score_tuple[2] == 10:
+                break
+            elif word_tuple[2] < highest_score_tuple[2]:
+                highest_score_tuple = word_tuple
+                highest_score = word_tuple[1]
+        else:
+            continue
+    return highest_score_tuple
