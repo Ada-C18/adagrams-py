562147e4493875f99bbf385c09d54aa5540e820d
Maria Silva <hello@mariasilva.dev>
2022-09-28 14:38:56 -0500

    refactored wave 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -60,7 +60,6 @@ def score_word(word):
     point_dict = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F" : 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
 
     score = 0 
-    length_points = 8
 
     for char in word:
         if char in point_dict:
@@ -74,52 +73,34 @@ def score_word(word):
 
 
 def get_highest_word_score(word_list):
-    
-    final_tuple = ()
 
     final_dict = {}
 
     for word in word_list:
         word = word.upper()
         word_score = score_word(word)
-        print(f"the word is {word}, the score is {word_score}")
         final_dict[word] = word_score
 
-    the_winner = ""
-    winner_score = 0
-    for word in final_dict:
-        if len(word) == 10:
-            the_winner = word
-            winner_score = final_dict[word]
-            break
-        return the_winner, winner_score
-
     winner_score = max(final_dict.values())
-    print("winner score is ", winner_score)
     winning_words = []
 
     for key in final_dict:
         if winner_score == final_dict[key]:
-            print("winner score is", winner_score)
-            print("final_dict[key] is", final_dict[key])
             winning_words.append(key)
-            print(winning_words)
 
-    current_length = 10 #len(winning_words[0])
+    current_length = 10 
+    the_winner = ""
+    winner_score = 0
 
     for word in winning_words:
-        print("the word is ", word)
-        
-        # if len(word) == 10:
-        #     the_winner = word
-        # else:
-        if len(word) < current_length:
-            current_length = len(word)
+        if len(word) == 10:
             the_winner = word
-    print("result: ", the_winner , winner_score)
+            winner_score = final_dict[word]
+            return the_winner , winner_score
+        else:
+            if len(word) < current_length:
+                current_length = len(word)
+                the_winner = word
+                winner_score = final_dict[word]
+
     return the_winner , winner_score
-
-words = ["X", "XX", "XXX", "XXXX"]
-
-# Act
-best_word = get_highest_word_score(words)
