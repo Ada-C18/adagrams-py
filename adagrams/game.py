f3eb3736a829080d36aa686081dacaadb4bae48e
Lynn <lynn@jansheski.com>
2022-09-28 16:55:31 -0700

    cleaned up print statements and other misc comments

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,6 +59,7 @@ LETTER_VALUES = {
     'Y': 4, 
     'Z': 10 
 }
+
 def draw_letters():
     letters = []
     while len(letters) < 10:
@@ -107,73 +108,33 @@ def get_highest_word_score(word_list):
         score = score_word(word)
         words_with_scores[word] = score
     
-    highest_score_word = max(words_with_scores,key=words_with_scores.get)
+    highest_score_word = max(words_with_scores,key=words_with_scores.get) 
     high_score = words_with_scores[highest_score_word]
-    print(high_score)
-    # print(max_score)
+
     max_score_tuple = (highest_score_word, words_with_scores[highest_score_word])
     
-
     multiple_max_score_words = []
     for key in words_with_scores:
         if high_score == words_with_scores[key]:
             multiple_max_score_words.append(key)
-    print(f"{multiple_max_score_words=}")
 
     if len(multiple_max_score_words) == 1:
         current_winner = max_score_tuple
     else:
-
         for i in range(0, len(multiple_max_score_words)):
-            print((multiple_max_score_words[i]))
-            print(len(multiple_max_score_words[i]))
             if len(multiple_max_score_words[i]) == 10:
                 return (multiple_max_score_words[i], words_with_scores[multiple_max_score_words[i]])
-                print(len(multiple_max_score_words[i]))  
-
             elif len(multiple_max_score_words[i-1]) < len(multiple_max_score_words[i]):
                 current_winner = (multiple_max_score_words[i-1], words_with_scores[multiple_max_score_words[i-1]]) 
-
             else:
                 current_winner = (multiple_max_score_words[i], words_with_scores[multiple_max_score_words[i]])
 
     return current_winner   
 
-
-
-    # if len(multiple_max_score_words) < 2:
-    #     return max_score_word
-    # else:
-    #     return multiple_max_score_words
     
 
 
 
 
-
-
-# ### Wave 4: get_highest_word_score
-
-# After several hands have been drawn, words have been submitted, checked, 
-# scored, and played, you need a way to find the highest scoring word. This 
-# function looks at the list of `word_list` and calculates which of these 
-# words has the highest score, applies any tie-breaking logic, and returns 
-# the winning word in a special data structure.
-
-# Implement a function called `get_highest_word_score` in `game.py`. This method 
-# should have the following properties:
-
-# - Has one parameter: `word_list`, which is a list of strings
-# - Returns a tuple that represents the data of a winning word and it's score.  
-# The tuple must contain the following elements:
-#   - index 0 ([0]): a string of a word
-#   - index 1 ([1]): the score of that word
-# - In the case of tie in scores, use these tie-breaking rules:
-#     - prefer the word with the fewest letters...
-#     - ...unless one word has 10 letters. If the top score is tied between 
-# multiple words and one is 10 letters long, choose the one with 10 letters 
-# over the one with fewer tiles
-#     - If the there are multiple words that are the same score and the same 
-# length, pick the first one in the supplied list
 
 
