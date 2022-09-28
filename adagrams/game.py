c06d8671f8a50ee1271c8ef7a66f099b14299b9a
tiffany <tiffanyrogers.dev@gmail.com>
2022-09-28 17:50:13 -0400

    Function 3 done and green

--- adagrams/game.py
+++ adagrams/game.py
@@ -5,7 +5,7 @@ tiles_org = ["A", "A", "A","A", "A", "A","A", "A", "A","B", "B", "C", "C", 'D', 
 
 
 def draw_letters():
-    tiles=tiles_org.copy()
+    tiles = tiles_org.copy()
     hand = []
 
     for letter in range(10):
@@ -14,13 +14,60 @@ def draw_letters():
         tiles.remove(tiles[tile])
     return hand
 
-# def uses_available_letters(word, letter_bank):
-#     pass
+def uses_available_letters(word, letter_bank):
+    correct_letters = []
+    for char in word:
+        if char in letter_bank:
+            correct_letters.append(char)
+            letter_bank.remove(char)
+    if len(word) == len(correct_letters):
+        return True
+    else:
+        return False
+    
+    
 
-# def score_word(word):
-#     pass
+def score_word(word):
+        tile_points = {
+        'A': 1,
+        'E': 1,
+        'I': 1,
+        'O': 1,
+        'U': 1,
+        'L': 1,
+        'N': 1,
+        'R': 1,
+        'S': 1,
+        'T': 1,
+        'D': 2,
+        'G': 2,
+        'B': 3,
+        'C': 3,
+        'M': 3,
+        'P': 3,
+        'F': 4,
+        'H': 4,
+        'V': 4,
+        'W': 4,
+        'Y': 4,
+        'K': 5,
+        'J': 8,
+        'X': 8,
+        'Q': 10,
+        'Z': 10
+        }
+        word_points = 0
+        upper_word = word.upper()
+        for letter in upper_word:
+            if letter in tile_points:
+                word_points += tile_points[letter]
+        if len(upper_word) >= 7:
+            word_points += 8
+            
+        return word_points        
 
-# def get_highest_word_score(word_list):
-#     pass
+def get_highest_word_score(word_list):
+    pass
 
 #this is a message
+    pass
