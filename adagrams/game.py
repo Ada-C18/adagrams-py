757054c8502312faab8b991bd9ccfcc6dd400c4d
Ariel <gabriellaw15@gmail.com>
2022-09-28 13:28:31 -0700

    Cleaned up comments in game.py file and refactored wave function

--- adagrams/game.py
+++ adagrams/game.py
@@ -40,9 +40,7 @@ def draw_letters():
 
         if random_letter in hand:
             letter_count = hand.count(random_letter)
-
             if letter_count < pool[random_letter]:
-                # append letter to list
                 hand.append(random_letter)
             else:
                 continue
@@ -53,14 +51,12 @@ def draw_letters():
 
 # WAVE 2
 def uses_available_letters(word, letter_bank):
-    # 1. Turn letter_bank into a dict (letter: frq)
     word_dict = {}
     letter_bank_dict = {}
 
     for letter in letter_bank:
         letter_bank_dict[letter] = letter_bank.count(letter)
 
-    # 2. Turn word into dict (letter: frq)
     word = word.upper()
 
     for letter in word:
@@ -68,7 +64,6 @@ def uses_available_letters(word, letter_bank):
         if letter not in letter_bank_dict:
             return False
 
-    # 3. Create a for loop to compare freq
     for letter, freq in word_dict.items():
         if freq <= letter_bank_dict[letter]:
             return True
@@ -78,7 +73,6 @@ def uses_available_letters(word, letter_bank):
 
 # WAVE 3
 def score_word(word):
-    # 1. Create score chart dict (score as key, letters as list in value)
     score_chart = {
         1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
         2: ["D", "G"],
@@ -88,20 +82,18 @@ def score_word(word):
         8: ["J", "X"],
         10: ["Q", "Z"],
     }
-    # 2. Create a variable for points which will be returned
+
     points = 0
-    # 3. Loop through each letter in word
+
     word = word.upper()
     for letter in word:
         for point, letter_list in score_chart.items():
             if letter in letter_list:
                 points += point
 
-    # 4.     Find letter in score_dict and add key to points
-    # 5. If length of word is greater than 7 and less than 10, add 8 to total points
     if len(word) > 6 and len(word) < 11: ##### MADE CHANGE HEREEEEEE #######
         points += 8
-    # 6. Return points
+
     return points
 
 
@@ -112,16 +104,12 @@ def get_highest_word_score(word_list):
     Output: a tuple with (word str, score of that word)
     '''
 
-    # 1. Create a dict for word scores
     word_score_dict = {}
 
-    # 2. Calculate score for each word in word_list with score_word function
     for word in word_list:
         score = score_word(word)
-    # 3. Add score to words_score_dict
         word_score_dict[word] = score
 
-    # 4. Find highest score
     highest_score = 0
     highest_score_words = []
 
@@ -132,29 +120,23 @@ def get_highest_word_score(word_list):
         elif score == highest_score:
             highest_score_words.append(word)
 
-    # print(f"highest_score {highest_score}")
-    # print(f"highest_score_words {highest_score_words}")
-
     if len(highest_score_words) == 1:
         return (highest_score_words[0], highest_score)
     elif len(highest_score_words) == 0:
         return None
-
-    # 5. If multiple highest -> Tie breaking rule
-    # else:
-    # -- if len(word1) == len(word2), return word1
-    if len(highest_score_words[0]) == len(highest_score_words[1]):
-        return (highest_score_words[0], highest_score)
     else:
-        # -- if len(word) == 10, return word with 10 letters
-        if len(highest_score_words[0]) == 10:
-            return (highest_score_words[0], highest_score)
+        first_highest = highest_score_words[0]
+        second_highest = highest_score_words[1]
 
-        elif len(highest_score_words[1]) == 10:
-            return (highest_score_words[1], highest_score)
+        if len(first_highest) == len(second_highest):
+            return (first_highest, highest_score)
+        else:
+            if len(first_highest) == 10:
+                return (first_highest, highest_score)
+            elif len(second_highest) == 10:
+                return (second_highest, highest_score)
 
-    # -- compare len of words, return word with fewest letter
-    if len(highest_score_words[0]) < len(highest_score_words[1]):
-        return (highest_score_words[0], highest_score)
-    elif len(highest_score_words[1]) < len(highest_score_words[0]):
-        return (highest_score_words[1], highest_score)
+        if len(first_highest) < len(second_highest):
+            return (first_highest, highest_score)
+        elif len(second_highest) < len(first_highest):
+            return (second_highest, highest_score)
