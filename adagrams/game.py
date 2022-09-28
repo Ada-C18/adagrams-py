f670f0fd6eab98c8ec2cce2f506d3c972f6376dd
sabraha0311 <sabraha0311@gmail.com>
2022-09-28 13:38:26 -0700

    Wave 3 completed

--- adagrams/game.py
+++ adagrams/game.py
@@ -62,7 +62,17 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    letter_value = {"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, "K": 5, "J": 8, "X": 8, "Q": 10, "Z": 10}
+
+    point = 0
+    for letter in word.upper():
+        if letter_value[letter]:
+            point += letter_value[letter]
+
+    if len(word) >= 7:
+        point += 8
+    return point
+
 
 def get_highest_word_score(word_list):
     pass
