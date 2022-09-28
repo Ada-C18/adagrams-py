ef2520e73494e52faf2770bca24c8a44b03ce0b4
Intesar Siraj <ghideruk@gmail.com>
2022-09-28 14:02:12 -0700

    wave 3 done

--- adagrams/game.py
+++ adagrams/game.py
@@ -60,11 +60,21 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
-    # create the dictionary for letter in word (score_chart)
-    # score is starting from 0
+    LETTER_SCORE = {'A': 1,'B': 3,'C': 3,'D': 2,'E': 1,'F': 4,'G': 2,'H': 4,'I': 1,'J': 8,'K': 5, 
+    'L': 1,'M': 3,'N': 1,'O': 1,'P': 3,'Q': 10,'R': 1,'S': 1,'T': 1,'U': 1,'V': 4,'W': 4,'X': 8,'Y': 4,'Z':10}
+    word_upper = word.upper()
+    score = 0
+        
+    for letter in word_upper:
+        if letter in LETTER_SCORE:
+            score += LETTER_SCORE[letter]
+    if len(word_upper) >= 7 and len(word_upper) <= 10:
+            score += 8
+    return score
+
     # iterate over the word and add scores, value = dict["keys"]
     # check len(word) == 7, 8, 9, or 10, then score += 8 points
+
 
 def get_highest_word_score(word_list):
     pass
