92b4d629fd0b00a02d17d33a73bf9c1cf42339ce
Aisha Husseini <ayasha.husseini@gmail.com>
2022-09-27 20:37:32 -0400

    passed wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -48,19 +48,65 @@ def draw_letters():
 
 def uses_available_letters(word, letter_bank):
 
-    word_2 = word.upper()
+    case_word = word.upper()
     letter_bank_copy = copy.deepcopy(letter_bank)
-    for letter in word_2:
+    for letter in case_word:
         if letter.upper() not in letter_bank_copy:
             return False
-        elif letter.upper() in word_2:
+        elif letter.upper() in case_word:
             letter_bank_copy.remove(letter)
     return True
     
 
 def score_word(word):
     
-    pass
+    SCORE_DICT = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+
+    case_word = word.upper()
+    sum_list = []
+    for letter in case_word:
+        for key, value in SCORE_DICT.items():
+            if letter == key:
+                sum_list.append(value)
+    n = len(case_word)
+    if n == 7 or n==8 or n==9 or n==10:
+        sum_list.append(8)
+    return sum(sum_list)
 
 def get_highest_word_score(word_list):
-    pass
+    word_and_score = []
+    highest_score = 0 
+    for word in word_list:
+        if score_word(word) > highest_score:
+            highest_score = score_word(word)
+            word_and_score
+
+
+    
