98cac416a1c91cf3041c719224a1e2eae9020671
Emma Kizil <fmodabber@outlook.com>
2022-09-29 10:13:54 -0700

    added const_dicts file

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,33 +1,5 @@
 import random
-
-LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
+from adagrams.const_dicts import *
 
 def draw_letters():
     letter_pool_list = []
@@ -53,39 +25,11 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     word = word.upper()
-    score_chart_dict = {
-        "A": 1,
-        "B": 3,
-        "C": 3,
-        "D": 2, 
-        "E": 1,
-        "F": 4,
-        "G": 2,
-        "H": 4,
-        "I": 1,
-        "J": 8,
-        "K": 5,
-        "L": 1,
-        "M": 3,
-        "N": 1,
-        "O": 1,
-        "P": 3,
-        "Q": 10,
-        "R": 1,
-        "S": 1,
-        "T": 1,
-        "U": 1,
-        "V": 4,
-        "W": 4,
-        "X": 8,
-        "Y": 4,
-        "Z": 10,
-    }
     sum = 0
     if len(word) >= 7:
         sum += 8
     for letter in word:
-        sum += score_chart_dict[letter]
+        sum += SCORE_CHART_DICT[letter]
     return sum
 
 def get_highest_word_score(word_list):
@@ -102,7 +46,4 @@ def get_highest_word_score(word_list):
                 continue
             elif len(word) < len(highest_score_word) or len(word) == 10:
                 highest_score_word = word
-            # elif len(word) == 10:
-            #     highest_score_word = word
-    
     return (highest_score_word, highest_score)
