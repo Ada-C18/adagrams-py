aa5e71bca3354e01ff58978f1b7278b387b64739
Siyi Ji <monkeyqueenstacy@gmail.com>
2022-09-29 17:06:52 -0400

    wave 2 fixed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 
-=======
 import random
->>>>>>> 34dc305fe6282a68e7664a420fe0bd34fb8fb087
 def draw_letters():
     distribution_of_letters = {"A" : 9, "N" : 6, "B" : 2, "O" : 8, "C" : 2, "P" : 2, "D" : 4 ,"Q" : 1, "E" : 12, "R" : 6, "F" : 2, "S" : 4, "G" : 3, "T" : 6, "H" : 2, "U" : 4, "I" : 9, "V" : 2, "J" : 1 , "W" : 2, "K" : 1, "X" : 1, "L" : 4, "Y" : 2, "M" : 2, "Z" : 1}
     letters = []
@@ -14,17 +11,19 @@ def draw_letters():
     return letters
 
 def uses_available_letters(word, letter_bank):
-    available = True
-    counter = 0
-    for letter in word:
-        for lb in letter_bank:
-            if letter == lb: 
-                counter += 1
-                #letter_bank.remove(lb)
-    if len(word) == counter: 
-            return True
-    else: 
-    	return False
+    counter = {}
+    for letter in letter_bank:
+        if letter in counter:
+            counter[letter] += 1
+        else:
+            counter[letter] = 1
+    
+    for ch in word.upper():
+            if ch in counter and counter[ch] > 0:
+                counter[ch] -= 1
+            else:
+                return False
+    return True
 
 def score_word(word):
     point_1_letters = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
