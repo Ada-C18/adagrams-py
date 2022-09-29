1f1541e1dcdf217d7afa2b486ee4e5bd42bd0554
Luciane Dias <lucilewa@yahoo.com>
2022-09-29 16:22:10 -0700

    completed wave 3 started wave 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -28,7 +28,34 @@ LETTER_POOL = {
     'Y': 2, 
     'Z': 1
 }
-
+SCORE_CHART =  {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
 def create_bag_of_letters():
     """
     Returns an array containing all the letters of the "LETTER_POOL" dictionary 
@@ -74,10 +101,44 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    final_score = 0
+    upper_case_word = word.upper()
+    for char in upper_case_word:
+        final_score += SCORE_CHART[char]
+    if len(word) > 6 and len(word) < 11:
+        final_score += 8
+    return final_score
+    
 
 def get_highest_word_score(word_list):
-    pass
+    word_info = []
+    for word in word_list:
+        word_dict = []
+        word_dict["word"] = word
+        word_dict["score"] = score_word(word)
+        word_dict["length"] = len(word)
+        word_info.append(word_dict)
+    
+    max_words = []
+    max_score = 0
+
+    for word_dict in word_info:
+        if word_dict ["score"] ==  max_score:
+            max_words.append(word_dict ["word"])
+        elif word_dict ["score"] > max_score: 
+            max_score =  word_dict ["score"]
+            max_words.clear()
+            max_words.append(word_dict ["word"])
+    if len(max_words) == 1:
+        result = (max_words[0], score_word[max_words[0]])
+        return result
+
+
+# word_info:
+# {“word”: word,
+# “Score”: score_word(word),
+# “Length”: len(word)
+# }
 
 #hand = ['S', 'B', 'E', 'T', 'R','E' , 'Z', 'A', 'L', 'M']
 #uses_available_letters("TREE", hand)
