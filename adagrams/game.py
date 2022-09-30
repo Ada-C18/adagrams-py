02858180e09b512735eb45ca1211765c0f3790aa
Nashwa <n.alsharki@gmail.com>
2022-09-30 13:09:14 -0400

    refactored code

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,15 +1,38 @@
 import random
 
-def draw_letters():
-    letter_dict = {
-                "A" : 9 , "N" : 6, "B" : 2, "O" : 8, "C" : 2, "P" : 2, "D" : 4, "Q" : 1,
-                "E" : 12, "R" : 6, "F": 2, "S" : 4, "G" : 3, "T" : 6, "H" : 2, "U" : 4,
-                "I" : 9, "V" : 2, "J" : 1, "W" : 2, "K" : 1, "X" : 1, "L" : 4, "Y" : 2,
-                "M" : 2, "Z" : 1 
-                }     
+letter_dict = { 
+                'A': {'frequency': 9, 'score': 1}, 
+                'B': {'frequency': 2, 'score': 3}, 
+                'C': {'frequency': 2, 'score': 3}, 
+                'D': {'frequency': 4, 'score': 2}, 
+                'E': {'frequency': 12, 'score': 1}, 
+                'F': {'frequency': 2, 'score': 4}, 
+                'G': {'frequency': 3, 'score': 2}, 
+                'H': {'frequency': 2, 'score': 4}, 
+                'I': {'frequency': 9, 'score': 1}, 
+                'J': {'frequency': 1, 'score': 8}, 
+                'K': {'frequency': 1, 'score': 5}, 
+                'L': {'frequency': 4, 'score': 1}, 
+                'M': {'frequency': 2, 'score': 3}, 
+                'N': {'frequency': 6, 'score': 1},
+                'O': {'frequency': 8, 'score': 1}, 
+                'P': {'frequency': 2, 'score': 3}, 
+                'Q': {'frequency': 1, 'score': 10}, 
+                'R': {'frequency': 6, 'score': 1}, 
+                'S': {'frequency': 4, 'score': 1}, 
+                'T': {'frequency': 6, 'score': 1}, 
+                'U': {'frequency': 4, 'score': 1}, 
+                'V': {'frequency': 2, 'score': 4}, 
+                'W': {'frequency': 2, 'score': 4}, 
+                'X': {'frequency': 1, 'score': 8}, 
+                'Y': {'frequency': 2, 'score': 4}, 
+                'Z': {'frequency': 1, 'score': 10}
+                }
+    
+def draw_letters():  
     letter_pool = []
     for letter in letter_dict:
-        letter_pool += list(letter) * letter_dict[letter]
+        letter_pool += list(letter) * letter_dict[letter]['frequency']
     
     player_hand = []
     while len(player_hand) < 10:
@@ -28,17 +51,7 @@ def uses_available_letters(word, letter_bank):
     return True          
 
 def score_word(word):
-    letter_scores = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1,
-                     'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3,
-                     'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-                     'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10
-                     }
-    total_points = 0
-    for letter in word.upper():
-        try:
-            total_points += letter_scores[letter]
-        except:
-            pass
+    total_points = sum([letter_dict[letter]['score'] for letter in word.upper()])
     
     if len(word) >= 7:
         total_points += 8
@@ -46,15 +59,15 @@ def score_word(word):
     return total_points
 
 def get_highest_word_score(word_list):
-    list_of_scores = [(word, score_word(word)) for word in word_list]
+    scores = [(word, score_word(word)) for word in word_list]
     
-    highest_score = list_of_scores[0]
-    for score in list_of_scores: 
+    highest_score = scores[0]
+    for score in scores: 
         if score[1] > highest_score[1]:
             highest_score = score
 
         if score[1] == highest_score[1]:
-            if (len(score[0]) < len(highest_score[0]) or len(score[0]) == 10) and len(highest_score[0]) < 10:
+            if len(highest_score[0]) < 10 and (len(score[0]) == 10 or len(score[0]) < len(highest_score[0])):
                 highest_score = score
                 
     return highest_score
