9e8d45b1a4dde47c760d5804042d7d16f9cc8d7c
Irene <choireneh@gmail.com>
2022-09-29 23:11:12 -0400

    Added score_word function and SCORE_CHART constant

--- adagrams/game.py
+++ adagrams/game.py
@@ -27,6 +27,35 @@ LETTER_POOL = {
     'Y': 2, 
     'Z': 1
 }
+
+SCORE_CHART = {
+    'A': 1,
+    'E': 1,
+    'I': 1,
+    'O': 1,
+    'U': 1,
+    'L': 1,
+    'N': 1,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'D': 2,
+    'G': 2,
+    'B': 3,
+    'C': 3,
+    'M': 3,
+    'P': 3,
+    'F': 4,
+    'H': 4,
+    'V': 4,
+    'W': 4,
+    'Y': 4,
+    'K': 5,
+    'J': 8,
+    'X': 8,
+    'Q': 10,
+    'Z': 10,
+}
 def draw_letters():
     letter_list = []
     hand = []
@@ -52,7 +81,16 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    formatted_word = word.upper()
+    score = 0
+
+    for letter in formatted_word:
+        score += SCORE_CHART[letter]
+    
+    if len(word) >= 7:
+        score += 8
+    
+    return score
 
 def get_highest_word_score(word_list):
     pass
