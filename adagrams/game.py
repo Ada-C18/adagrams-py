cfa4b208e801f1fda8ca35b524c0f6d6aaf37da3
Irene <choireneh@gmail.com>
2022-09-29 23:20:23 -0400

    Added get_highest_word_score function

--- adagrams/game.py
+++ adagrams/game.py
@@ -93,4 +93,17 @@ def score_word(word):
     return score
 
 def get_highest_word_score(word_list):
-    pass
+    winner = [word_list[0], score_word(word_list[0])]
+    for word in word_list:
+        if score_word(word) > winner[1]:
+            winner = [word, score_word(word)]
+        if score_word(word) == winner[1]:
+            if len(winner[0]) == 10:
+                pass
+            elif len(word) == 10:
+                winner = [word, score_word(word)]
+            elif len(winner[0]) < len(word):
+                pass
+            elif len(word) < len(winner[0]):
+                winner = [word, score_word(word)]
+    return tuple(winner)
