fd2b6f966d9d54747e3596c686995689730cd717
Siyi Ji <monkeyqueenstacy@gmail.com>
2022-10-14 18:44:11 -0400

    fix problems according to feedback

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,10 @@
 
 import random
+import copy
+POOL_OF_LETTERS = {"A" : 9, "N" : 6, "B" : 2, "O" : 8, "C" : 2, "P" : 2, "D" : 4 ,"Q" : 1, "E" : 12, "R" : 6, "F" : 2, "S" : 4, "G" : 3, "T" : 6, "H" : 2, "U" : 4, "I" : 9, "V" : 2, "J" : 1 , "W" : 2, "K" : 1, "X" : 1, "L" : 4, "Y" : 2, "M" : 2, "Z" : 1}
+
 def draw_letters():
-    distribution_of_letters = {"A" : 9, "N" : 6, "B" : 2, "O" : 8, "C" : 2, "P" : 2, "D" : 4 ,"Q" : 1, "E" : 12, "R" : 6, "F" : 2, "S" : 4, "G" : 3, "T" : 6, "H" : 2, "U" : 4, "I" : 9, "V" : 2, "J" : 1 , "W" : 2, "K" : 1, "X" : 1, "L" : 4, "Y" : 2, "M" : 2, "Z" : 1}
+    distribution_of_letters = copy.deepcopy(POOL_OF_LETTERS)
     letters = []
     while len(letters) < 10:
         userletters = random.choice(list(distribution_of_letters.items()))
@@ -9,6 +12,11 @@ def draw_letters():
             letters.append(userletters[0])
             distribution_of_letters[userletters[0]]-=1
     return letters
+# solution 2(try random.sample)
+    # letter_list = list(distribution_of_letters.keys())
+    # letter_count = list(distribution_of_letters.values())
+    # letters = random.sample(letter_list, k = 10, counts = letter_count)
+    # return letters
 
 def uses_available_letters(word, letter_bank):
     counter = {}
@@ -26,59 +34,48 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    point_1_letters = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
-    point_2_letters = ["D", "G"]
-    point_3_letters = ["B", "C", "M", "P"]
-    point_4_letters = ["F", "H", "V", "W", "Y"]
-    point_5_letters = ["K"]
-    point_8_letters = ["J", "X"]
-    point_10_letters = ["Q", "Z"]
-    
     # use map to store letter to score relationships
     score_chart = {
-        1: point_1_letters,
-        2: point_2_letters,
-        3: point_3_letters,
-        4: point_4_letters,
-        5: point_5_letters,
-        8: point_8_letters,
-        10: point_10_letters
+        "AEIOULNRST": 1,
+        "DG": 2,
+        "BCMP": 3,
+        "FHVWY": 4,
+        "K": 5,
+        "JX": 8,
+        "QZ": 10
         }
     
     total_score = 0
-
-    if len(word) != 0:
-        special_length = [7, 8, 9, 10]
-
-        # add additional 8 points for words with special length
-        if len(word) in special_length:
-            total_score += 8
-        
-        # find the score for each letter in word, and add score to total_score
-        for point, letters in score_chart.items():
-            for letter in word.upper():
-                if letter in letters:
-                    total_score += point
+    # add additional 8 points for words with special length
+    if len(word) > 6 and len(word) < 11:
+        total_score += 8
+    # find the score for each letter in word, and add score to total_score
+    for letter, value in score_chart.items(): 
+        for ch in word.upper():
+            if ch in letter:
+                total_score += value
 
     return total_score
 
     
 def get_highest_word_score(word_list):
+    # Function iterate through the word_list, and update best_word when current word is the best word
+    # store the first word and score to the empty best_word
+    # update best_word with new value when current score is greater than previous best score
+    # update best_word when there's a tie and the previous word length is not 10 and either current word length is 10 or shorter than previous word length
     best_word = None
-
-    # iterate through the word_list, and update best_word when current word is the best word
+    
     for word in word_list: 
         score = score_word(word)
 
-        # store the first word and score to the empty best_word
         if not best_word:
             best_word = (word, score)
-        # update best_word with new value when current score is greater than previous best score
+        
         elif score > best_word[1]:
             best_word = (word, score)
-        # update best_word when there's a tie and the previous word length is not 10 and either current word length is 10 or shorter than previous word length
+        
         elif score == best_word[1] and len(best_word[0]) != 10 and (len(word) == 10 or len(word) < len(best_word[0])):
             best_word = (word, score)
-        
+          
     return best_word
             
